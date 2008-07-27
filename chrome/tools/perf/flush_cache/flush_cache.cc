ce73b27f5ed2a724568562632db8df8dfbc677d5
nsylvain@google.com <nsylvain@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-27 01:59:12 +0000

    Add missing perf/flush_cache directory.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/tools/perf/flush_cache/flush_cache.cc
@@ -0,0 +1,52 @@
+// Copyright 2008, Google Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//    * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//    * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//    * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// This little program attempts to flush the disk cache for some files.
+// It's useful for testing Chrome with a cold database.
+
+#include "base/string_util.h"
+#include "chrome/test/test_file_util.h"
+
+int main(int argc, const char* argv[]) {
+  if (argc <= 1) {
+    fprintf(stderr, "flushes disk cache for files\n");
+    fprintf(stderr, "usage: %s <filenames>\n", argv[0]);
+    return 1;
+  }
+
+  for (int i = 1; i < argc; ++i) {
+    std::wstring filename = NativeMBToWide(argv[i]);
+    if (!file_util::EvictFileFromSystemCache(filename.c_str())) {
+      fprintf(stderr, "Failed to evict %s from cache -- is it a directory?\n",
+              argv[i]);
+    }
+  }
+
+  return 0;
+}
