de7409a2530901d7edd22848b44442e36372c133
maruel@google.com <maruel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-28 20:05:39 +0000

    Fix a build issue with Vista. Stop cp.exe from trying to emulate POSIX security on top of NTFS.
    BUG=1157117
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/build/JSConfig/prebuild.bat
+++ webkit/build/JSConfig/prebuild.bat
@@ -6,6 +6,9 @@ set JSENG=%2
 set CYGWIN_ROOT=%~dp0..\..\..\third_party\cygwin\
 set GNU_ROOT=%~dp0..\..\..\third_party\gnu\files
 
+:: Fix cp.exe on vista: without this flag, the files that it creates are not accessible.
+set CYGWIN=nontsec
+
 set PATH=%CYGWIN_ROOT%bin;%GNU_ROOT%;%SystemRoot%;%SystemRoot%\system32
 
 :: Ensure that the cygwin mount points are defined
