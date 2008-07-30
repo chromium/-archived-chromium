dc2e49870b3a9c80277c6078b3b3ae25788995d2
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-07-30 17:47:30 +0000

    Use own our bzip2 error handler instead of the default which prints error messages to STDERR and calls exit(1).  This is better, since we will now crash instead of exiting.  This also removes some unnecessary code and error strings from bzip2.
    BUG=1289861
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121 0039d316-1c4b-4281-b951-d872f2087c98

--- base/bzip2_error_handler.cc
+++ base/bzip2_error_handler.cc
@@ -31,6 +31,7 @@
 
 // We define BZ_NO_STDIO in third_party/bzip2 to remove its internal STDERR
 // error reporting.  This requires us to export our own error handler.
+extern "C"
 void bz_internal_error(int errcode) {
   CHECK(false) << "bzip2 internal error: " << errcode;
 }
