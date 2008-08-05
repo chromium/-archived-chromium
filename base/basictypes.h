210e56f3b108a71bb3b3b6b3a2edb749a0b9d371
deanm@google.com <deanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-05 14:57:58 +0000

    Import base::LINKER_INITIALIZED from google-perftools / tcmalloc.  This is effectively just an annotation; a class can describe that it's ok to be used as a global static object because it will be zero initialized, so construction order doesn't matter.  This also makes it clear from someone using it as a global object that it's safe.  This will be used for spinlocks.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@374 0039d316-1c4b-4281-b951-d872f2087c98

--- base/basictypes.h
+++ base/basictypes.h
@@ -399,5 +399,22 @@ inline Dest bit_cast(const Source& source) {
   return dest;
 }
 
+// The following enum should be used only as a constructor argument to indicate
+// that the variable has static storage class, and that the constructor should
+// do nothing to its state.  It indicates to the reader that it is legal to
+// declare a static instance of the class, provided the constructor is given
+// the base::LINKER_INITIALIZED argument.  Normally, it is unsafe to declare a
+// static variable that has a constructor or a destructor because invocation
+// order is undefined.  However, IF the type can be initialized by filling with
+// zeroes (which the loader does for static variables), AND the destructor also
+// does nothing to the storage, AND there are no virtual methods, then a
+// constructor declared as
+//       explicit MyClass(base::LinkerInitialized x) {}
+// and invoked as
+//       static MyClass my_variable_name(base::LINKER_INITIALIZED);
+namespace base {
+enum LinkerInitialized { LINKER_INITIALIZED };
+}
+
 
 #endif  // BASE_BASICTYPES_H__
