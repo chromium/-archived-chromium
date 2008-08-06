2effba232988a24b36bbf4a473b3236a859577b5
nsylvain@google.com <nsylvain@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-06 17:48:24 +0000

    Revert change 408 to see if it fixes the ui tests.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@438 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/render_process_host.cc
+++ chrome/browser/render_process_host.cc
@@ -202,7 +202,7 @@ RenderProcessHost::~RenderProcessHost() {
   channel_.reset();
 
   if (process_.handle() && !run_renderer_in_process_) {
-    watcher_.StopWatching();
+    MessageLoop::current()->WatchObject(process_.handle(), NULL);
     ProcessWatcher::EnsureProcessTerminated(process_.handle());
   }
 
@@ -420,7 +420,7 @@ bool RenderProcessHost::Init() {
         process_.set_handle(process);
       }
 
-      watcher_.StartWatching(process_.handle(), this);
+      MessageLoop::current()->WatchObject(process_.handle(), this);
     }
   }
 
@@ -576,7 +576,7 @@ void RenderProcessHost::OnChannelConnected(int32 peer_pid) {
       // returned by CreateProcess() has to the process object.
       process_.set_handle(OpenProcess(MAXIMUM_ALLOWED, FALSE, peer_pid));
       DCHECK(process_.handle());
-      watcher_.StartWatching(process_.handle(), this);
+      MessageLoop::current()->WatchObject(process_.handle(), this);
     }
   } else {
     // Need to verify that the peer_pid is actually the process we know, if
@@ -590,6 +590,8 @@ void RenderProcessHost::OnObjectSignaled(HANDLE object) {
   DCHECK(process_.handle());
   DCHECK(channel_.get());
   DCHECK_EQ(object, process_.handle());
+
+  MessageLoop::current()->WatchObject(object, NULL);
 
   bool clean_shutdown = !process_util::DidProcessCrash(object);
 
