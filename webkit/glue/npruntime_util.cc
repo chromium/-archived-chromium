ffc5ebd326eb2eccfc72a37a2225762d3437d2dc
darin@google.com <darin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-06 19:33:04 +0000

    fix kjs build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@448 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/npruntime_util.cc
+++ webkit/glue/npruntime_util.cc
@@ -35,7 +35,8 @@
 #if USE(V8_BINDING)
 #include "webkit/port/bindings/v8/np_v8object.h"
 #elif USE(JAVASCRIPTCORE_BINDINGS)
-#include "c/c_utility.h"
+#include "bindings/c/c_utility.h"
+using KJS::Bindings::PrivateIdentifier;
 #endif
 
 #include "base/pickle.h"
