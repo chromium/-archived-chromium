9ca4f2e9111cf3817fd3eb360a831f6a1da052a6
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-07 18:32:14 +0000

    Remove the unit tests for the error condition. Vista handles the error differently, so we can't have a consistent repro case. I don't think this is important to test anyway, since the functions are defined to do whatever the OS does.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@525 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sys_string_conversions_win_unittest.cc
+++ base/sys_string_conversions_win_unittest.cc
@@ -43,7 +43,10 @@ TEST(SysStringsWin, SysWideToUTF8) {
   // Error case. When Windows finds a UTF-16 character going off the end of
   // a string, it just converts that literal value to UTF-8, even though this
   // is invalid.
-  EXPECT_EQ("\xE4\xBD\xA0\xED\xA0\x80zyxw", SysWideToUTF8(L"\x4f60\xd800zyxw"));
+  //
+  // This is what XP does, but Vista has different behavior, so we don't bother
+  // verifying it:
+  //EXPECT_EQ("\xE4\xBD\xA0\xED\xA0\x80zyxw", SysWideToUTF8(L"\x4f60\xd800zyxw"));
 
   // Test embedded NULLs.
   std::wstring wide_null(L"a");
@@ -65,7 +68,10 @@ TEST(SysStringsWin, SysUTF8ToWide) {
 
   // Error case. When Windows finds an invalid UTF-8 character, it just skips
   // it. This seems weird because it's inconsistent with the reverse conversion.
-  EXPECT_EQ(L"\x4f60zyxw", SysUTF8ToWide("\xe4\xbd\xa0\xe5\xa5zyxw"));
+  //
+  // This is what XP does, but Vista has different behavior, so we don't bother
+  // verifying it:
+  //EXPECT_EQ(L"\x4f60zyxw", SysUTF8ToWide("\xe4\xbd\xa0\xe5\xa5zyxw"));
 
   // Test embedded NULLs.
   std::string utf8_null("a");
