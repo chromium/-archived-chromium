384e786a32174c0cdf843538671244f1a99d9024
sidchat@google.com <sidchat@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-09 00:08:43 +0000

    Comment out the tests for now.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@609 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tabs/tab_dragging_test.cc
+++ chrome/browser/tabs/tab_dragging_test.cc
@@ -43,6 +43,7 @@
 #include "googleurl/src/gurl.h"
 #include "net/base/net_util.h"
 
+
 class TabDraggingTest : public UITest {
 protected:
   TabDraggingTest() {
@@ -50,6 +51,7 @@ protected:
   }
 };
 
+/*
 // Automated UI test to open three tabs in a new window, and drag Tab-1 into
 // the position of Tab-2.
 TEST_F(TabDraggingTest, Tab1Tab2) {
@@ -155,6 +157,7 @@ TEST_F(TabDraggingTest, Tab1Tab2) {
 
 // Drag Tab-1 into the position of Tab-3. 
 TEST_F(TabDraggingTest, Tab1Tab3) {
+  /*
   scoped_ptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
   scoped_ptr<WindowProxy> window(
@@ -260,3 +263,4 @@ TEST_F(TabDraggingTest, Tab1Tab3) {
   EXPECT_EQ(tab2_new_url.spec(), tab3_url.spec());
   EXPECT_EQ(tab3_new_url.spec(), tab1_url.spec());
 }
+*/
