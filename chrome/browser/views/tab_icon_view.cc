18b6445ec016270a987f4c15f77547596787c84b
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-24 18:43:07 +0000

    fix release bustage.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tab_icon_view.cc
+++ chrome/browser/views/tab_icon_view.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <windows.h>
+
 #include "chrome/browser/views/tab_icon_view.h"
 
 #include "base/file_util.h"
