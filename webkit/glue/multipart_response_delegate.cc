302f2fc3dee8d7a311e24b4b7688e0ef24ad6835
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-26 18:11:51 +0000

    need to include config.h first
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1386 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/multipart_response_delegate.cc
+++ webkit/glue/multipart_response_delegate.cc
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "config.h"
 #include <string>
 
-#include "config.h"
 #pragma warning(push, 0)
 #include "HTTPHeaderMap.h"
 #include "ResourceHandle.h"
