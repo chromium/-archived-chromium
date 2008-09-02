76173ec122468a83dceea5447cc51b24d6d11525
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-02 20:46:01 +0000

    Take out uneeded comment (and test public Rietveld instance!).
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@1657 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/safe_browsing_util_unittest.cc
+++ chrome/browser/safe_browsing/safe_browsing_util_unittest.cc
@@ -1,7 +1,6 @@
 // Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
 
 #include "base/logging.h"
 #include "base/sha2.h"
