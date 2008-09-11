d95b076fb315b725cc186e99b18b30f30d26264f
paulg@google.com <paulg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-11 23:37:54 +0000

    Port the thread local storage unittest to Mac and linux. Review URL: http://codereview.chromium.org/1963
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2101 0039d316-1c4b-4281-b951-d872f2087c98

--- base/thread_local_storage_unittest.cc
+++ base/thread_local_storage_unittest.cc
@@ -2,19 +2,56 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#if defined(OS_WIN)
 #include <windows.h>
 #include <process.h>
+#endif
 
+#include "base/simple_thread.h"
 #include "base/thread_local_storage.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-// ignore warnings about ptr->int conversions that we use when
+#if defined(OS_WIN)
+// Ignore warnings about ptr->int conversions that we use when
 // storing ints into ThreadLocalStorage.
 #pragma warning(disable : 4311 4312)
+#endif
 
-namespace {
-  class ThreadLocalStorageTest : public testing::Test {
-  };
+const int kInitialTlsValue = 0x5555;
+static ThreadLocalStorage::Slot tls_slot(base::LINKER_INITIALIZED);
+
+
+class ThreadLocalStorageRunner : public base::DelegateSimpleThread::Delegate {
+ public:
+  explicit ThreadLocalStorageRunner(int* tls_value_ptr)
+      : tls_value_ptr_(tls_value_ptr) {}
+
+  virtual ~ThreadLocalStorageRunner() {}
+
+  virtual void Run() {
+    *tls_value_ptr_ = kInitialTlsValue;
+    tls_slot.Set(tls_value_ptr_);
+
+    int *ptr = static_cast<int*>(tls_slot.Get());
+    EXPECT_EQ(ptr, tls_value_ptr_);
+    EXPECT_EQ(*ptr, kInitialTlsValue);
+    *tls_value_ptr_ = 0;
+
+    ptr = static_cast<int*>(tls_slot.Get());
+    EXPECT_EQ(ptr, tls_value_ptr_);
+    EXPECT_EQ(*ptr, 0);
+  }
+
+ private:
+  int* tls_value_ptr_;
+  DISALLOW_COPY_AND_ASSIGN(ThreadLocalStorageRunner);
+};
+
+
+void ThreadLocalStorageCleanup(void *value) {
+  int *ptr = reinterpret_cast<int*>(value);
+  if (ptr)
+    *ptr = kInitialTlsValue;
 }
 
 
@@ -25,61 +62,33 @@ TEST(ThreadLocalStorageTest, Basics) {
   EXPECT_EQ(value, 123);
 }
 
-const int kInitialTlsValue = 0x5555;
-static ThreadLocalStorage::Slot tls_slot(base::LINKER_INITIALIZED);
-
-unsigned __stdcall TLSTestThreadMain(void* param) {
-  // param contains the thread local storage index.
-  int *index = reinterpret_cast<int*>(param);
-  *index = kInitialTlsValue;
-
-  tls_slot.Set(index);
-
-  int *ptr = static_cast<int*>(tls_slot.Get());
-  EXPECT_EQ(ptr, index);
-  EXPECT_EQ(*ptr, kInitialTlsValue);
-  *index = 0;
-
-  ptr = static_cast<int*>(tls_slot.Get());
-  EXPECT_EQ(ptr, index);
-  EXPECT_EQ(*ptr, 0);
-  return 0;
-}
-
-void ThreadLocalStorageCleanup(void *value) {
-  int *ptr = reinterpret_cast<int*>(value);
-  if (ptr)
-    *ptr = kInitialTlsValue;
-}
-
-
 TEST(ThreadLocalStorageTest, TLSDestructors) {
   // Create a TLS index with a destructor.  Create a set of
   // threads that set the TLS, while the destructor cleans it up.
   // After the threads finish, verify that the value is cleaned up.
   const int kNumThreads = 5;
-  HANDLE threads[kNumThreads];
   int values[kNumThreads];
+  ThreadLocalStorageRunner* thread_delegates[kNumThreads];
+  base::DelegateSimpleThread* threads[kNumThreads];
 
   tls_slot.Initialize(ThreadLocalStorageCleanup);
 
   // Spawn the threads.
-  for (int16 index = 0; index < kNumThreads; index++) {
+  for (int index = 0; index < kNumThreads; index++) {
     values[index] = kInitialTlsValue;
-    void *argument = static_cast<void*>(&(values[index]));
-    unsigned thread_id;
-    threads[index] = reinterpret_cast<HANDLE>(
-      _beginthreadex(NULL, 0, TLSTestThreadMain, argument, 0, &thread_id));
-    EXPECT_NE(threads[index], (HANDLE)NULL);
+    thread_delegates[index] = new ThreadLocalStorageRunner(&values[index]);
+    threads[index] = new base::DelegateSimpleThread(thread_delegates[index],
+                                                    "tls thread");
+    threads[index]->Start();
   }
 
   // Wait for the threads to finish.
   for (int index = 0; index < kNumThreads; index++) {
-    DWORD rv = WaitForSingleObject(threads[index], 60*1000);
-    EXPECT_EQ(rv, WAIT_OBJECT_0);  // verify all threads finished
+    threads[index]->Join();
+    delete threads[index];
+    delete thread_delegates[index];
 
-    // verify that the destructor was called and that we reset.
+    // Verify that the destructor was called and that we reset.
     EXPECT_EQ(values[index], kInitialTlsValue);
   }
 }
-
