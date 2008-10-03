7ae9aab8b54da77b52a1781c38d53ee9b4a9b185
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-03 14:14:17 +0000

    Fixes for the webcore target.  Don't use another DerivedSources phase, just rely on the DerivedSources generated by the v8 jsbindings target. Review URL: http://codereview.chromium.org/6449
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@2830 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/webkit.xcodeproj/webcore_prebuild.sh
+++ webkit/webkit.xcodeproj/webcore_prebuild.sh
@@ -27,35 +27,11 @@ fi
 
 rmdir "${GENERATED_DIR}/include/v8/new"
 
-# TODO(mmentovai): fix this to not be so hokey - the Apple build expects
-# JavaScriptCore to be in a framework  This belongs in the JSConfig target,
-# which already does something similar
-mkdir -p "${GENERATED_DIR}/include/v8/JavaScriptCore"
-cp -p "${SRCROOT}/../third_party/WebKit/JavaScriptCore/bindings/npruntime.h" \
-      "${GENERATED_DIR}/include/v8/JavaScriptCore"
-
-export DerivedSourcesDir="${GENERATED_DIR}/DerivedSources/v8/WebCore"
-mkdir -p "${GENERATED_DIR}/DerivedSources/v8/WebCore"
-cd "${GENERATED_DIR}/DerivedSources/v8/WebCore"
-
-# export CREATE_HASH_TABLE="${SRCROOT}/../third_party/WebKit/JavaScriptCore/kjs/create_hash_table"
-# TODO(mmentovai): The above is normally correct, but create_hash_table wound
-# up without the svn:executable property set in our repository.  See the TODO
-# in jsbindings_prebuild.sh.
-export CREATE_HASH_TABLE="${GENERATED_DIR}/create_hash_table"
-
+# TODO(mmentovai): Am I still needed?
 ln -sfh "${SRCROOT}/../third_party/WebKit/WebCore" WebCore
-export WebCore="WebCore"
-export SOURCE_ROOT="${WebCore}"
-export PORTROOT="${SRCROOT}/port"
 
-export PUBLICDOMINTERFACES="${PORTROOT}/../pending/PublicDOMInterfaces.h"
-make -f "${SRCROOT}/pending/DerivedSources.make" \
-     -j $(/usr/sbin/sysctl -n hw.ncpu)
-
-# Allow framework-style #imports of <WebCore/whatever.h> to find the right
-# headers.
-cd ..
+# TODO(mmentovai): If I'm still needed, can I move to jsbindings_prebuild.sh?
+cd "${GENERATED_DIR}/DerivedSources/v8"
 mkdir -p ForwardingHeaders/loader
 ln -sfh "${SRCROOT}/../third_party/WebKit/WebCore/loader" \
         ForwardingHeaders/loader/WebCore
