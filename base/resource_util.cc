f778b7df870d3258042d3c0f0c130c669f54f1b0
cpu@google.com <cpu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-08 19:29:12 +0000

    Klocwork bug. Checking the wrong variable. BUG = 3103
    Review URL: http://codereview.chromium.org/6453
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3030 0039d316-1c4b-4281-b951-d872f2087c98

--- base/resource_util.cc
+++ base/resource_util.cc
@@ -2,9 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/resource_util.h"
-
 #include "base/logging.h"
+#include "base/resource_util.h"
 
 namespace base {
 bool GetDataResourceFromModule(HMODULE module, int resource_id,
@@ -12,20 +11,26 @@ bool GetDataResourceFromModule(HMODULE module, int resource_id,
   if (!module)
     return false;
 
-  // Get a pointer to the data in the dll.
-  DCHECK(IS_INTRESOURCE(resource_id));
+  if (!IS_INTRESOURCE(resource_id)) {
+    NOTREACHED();
+    return false;
+  }
+
   HRSRC hres_info = FindResource(module, MAKEINTRESOURCE(resource_id),
                                  L"BINDATA");
   if (NULL == hres_info)
     return false;
 
   DWORD data_size = SizeofResource(module, hres_info);
-
   HGLOBAL hres = LoadResource(module, hres_info);
-  if (!hres_info)
+  if (!hres)
     return false;
 
-  *data = LockResource(hres);
+  void* resource = LockResource(hres);
+  if (!resource)
+    return false;
+
+  *data = resource;
   *length = static_cast<size_t>(data_size);
   return true;
 }
