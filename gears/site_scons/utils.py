e77074d7a80a937fa1ac77d7ed2857760b4bf6f1
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-10 00:01:37 +0000

    Pull 'common' outputs out into their own SConscript so they have their own variant dir.  Add ipc_test as a common output.
    Also fixed up some hackery around manipulating OUTDIRs.
    
    Review URL: http://codereview.chromium.org/7037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3158 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ gears/site_scons/utils.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2008 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+def PatternRule(target, source, env):
+  """Apply env substitution to a target with $SOURCE included.  Returns a list
+  containing the new target and source to pass to a builder."""
+  target_sub = env.subst(target, source=env.File(source))
+  return [target_sub, source]
