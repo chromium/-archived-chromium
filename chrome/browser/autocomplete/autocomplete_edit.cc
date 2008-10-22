bb9093a32a92838ff8da86ce9e315e8f54127d9f
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-22 23:48:34 +0000

    Change a dependency from clipboard_util.h to the more cross-platform clipboard.h
    Review URL: http://codereview.chromium.org/7884
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3797 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_edit.cc
+++ chrome/browser/autocomplete/autocomplete_edit.cc
@@ -7,7 +7,7 @@
 #include <locale>
 
 #include "base/base_drag_source.h"
-#include "base/clipboard_util.h"
+#include "base/clipboard.h"
 #include "base/gfx/skia_utils.h"
 #include "base/iat_patch.h"
 #include "base/ref_counted.h"
@@ -2271,7 +2271,7 @@ std::wstring AutocompleteEditView::GetClipboardText() const {
   // and pastes from the URL bar to itself, the text will get fixed up and
   // cannonicalized, which is not what the user expects.  By pasting in this
   // order, we are sure to paste what the user copied.
-  if (clipboard->IsFormatAvailable(ClipboardUtil::GetUrlWFormat()->cfFormat)) {
+  if (clipboard->IsFormatAvailable(Clipboard::GetUrlWFormatType())) {
     std::string url_str;
     clipboard->ReadBookmark(NULL, &url_str);
     // pass resulting url string through GURL to normalize
