fabe0c9c8e04a97c99180e66c5ec34a26ac1948c
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-23 18:26:43 +0000

    Make sure that checked state on {radiobox, checkbox} does not get flipped in xp style on a readonly control.
    http://code.google.com/p/chromium/issues/detail?id=1542
    
    Review URL: http://codereview.chromium.org/7897
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@3828 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/rendering/RenderThemeWin.cpp
+++ webkit/port/rendering/RenderThemeWin.cpp
@@ -414,16 +414,22 @@ bool RenderThemeWin::supportsFocusRing(const RenderStyle* style) const
 unsigned RenderThemeWin::determineState(RenderObject* o)
 {
     unsigned result = TS_NORMAL;
-    if (!isEnabled(o))
+    if (!isEnabled(o)) {
         result = TS_DISABLED;
-    else if (isReadOnlyControl(o))
-        result = ETS_READONLY; // Readonly is supported on textfields.
-    else if (isPressed(o)) // Active overrides hover and focused.
+    } else if (isReadOnlyControl(o)) {
+        if (o->style()->appearance() == CheckboxAppearance ||
+            o->style()->appearance() == RadioAppearance) {
+            result = TS_DISABLED;
+        } else if (o->style()->appearance() == TextFieldAppearance) {
+            result = ETS_READONLY; // Readonly is supported on textfields.
+        }
+    } else if (isPressed(o)) { // Active overrides hover and focused.
         result = TS_PRESSED;
-    else if (supportsFocus(o->style()->appearance()) && isFocused(o))
+    } else if (supportsFocus(o->style()->appearance()) && isFocused(o)) {
         result = ETS_FOCUSED;
-    else if (isHovered(o))
+    } else if (isHovered(o)) {
         result = TS_HOT;
+    }
     if (isChecked(o))
         result += 4; // 4 unchecked states, 4 checked states.
     return result;
