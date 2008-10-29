c3438fe52659beb668406cb12328bc1e0bd50924
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-29 23:34:36 +0000

    Fix more problems with search providers.
    onet.pl apparently moved their search service out of beta, and changed the URLs accordingly, since the time I put the original list together.
    
    In the other two cases, changing encodings to UTF-8 seemed to fix issues with words in the local languages, and who am I to argue with the results.
    
    BUG=3804,3809,3816
    Review URL: http://codereview.chromium.org/8719
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4190 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/template_url_prepopulate_data.cc
+++ chrome/browser/template_url_prepopulate_data.cc
@@ -362,7 +362,7 @@ const PrepopulatedEngine delfi_lv = {
   L"delfi.lv",
   L"http://smart.delfi.lv/img/smart_search.png",
   L"http://smart.delfi.lv/i.php?enc={inputEncoding}&q={searchTerms}",
-  "windows-1257",
+  "UTF-8",
   NULL,
   45,
 };
@@ -1402,8 +1402,8 @@ const PrepopulatedEngine ok = {
 const PrepopulatedEngine onet = {
   L"Onet.pl",
   L"onet.pl",
-  L"http://szukaj.beta.onet.pl/favicon.ico",
-  L"http://szukaj.beta.onet.pl/query.html?qt={searchTerms}",
+  L"http://szukaj.onet.pl/favicon.ico",
+  L"http://szukaj.onet.pl/query.html?qt={searchTerms}",
   "ISO-8859-2",
   NULL,
   75,
@@ -1485,7 +1485,7 @@ const PrepopulatedEngine rediff = {
   L"rediff.com",
   L"http://search1.rediff.com/favicon.ico",
   L"http://search1.rediff.com/dirsrch/default.asp?MT={searchTerms}",
-  "ISO-8859-1",
+  "UTF-8",
   NULL,
   37,
 };
@@ -3008,7 +3008,7 @@ void RegisterUserPrefs(PrefService* prefs) {
 }
 
 int GetDataVersion() {
-  return 17;  // Increment this if you change the above data in ways that mean
+  return 18;  // Increment this if you change the above data in ways that mean
               // users with existing data should get a new version.
 }
 
