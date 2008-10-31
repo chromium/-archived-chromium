88d99f62a068d76e1ed25b89d08988b3528d316f
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-31 00:03:09 +0000

    Fix delete key and other editor keyboard shortcuts on the Mac.
    Review URL: http://codereview.chromium.org/8930
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4258 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/editor_client_impl.cc
+++ webkit/glue/editor_client_impl.cc
@@ -17,6 +17,9 @@ MSVC_PUSH_WARNING_LEVEL(0);
 #include "EventNames.h"
 #include "HTMLInputElement.h"
 #include "Frame.h"
+#if defined(OS_MACOSX) || defined(OS_LINUX)
+#include "KeyboardCodes.h"
+#endif 
 #include "KeyboardEvent.h"
 #include "PlatformKeyboardEvent.h"
 #include "PlatformString.h"
@@ -362,6 +365,13 @@ void EditorClientImpl::redo() {
 static const unsigned CtrlKey = 1 << 0;
 static const unsigned AltKey = 1 << 1;
 static const unsigned ShiftKey = 1 << 2;
+static const unsigned MetaKey = 1 << 3;
+#if defined(OS_MACOSX)
+// Aliases for the generic key defintions to make kbd shortcuts definitions more
+// readable on OS X.
+static const unsigned OptionKey  = AltKey;
+static const unsigned CommandKey = MetaKey;
+#endif
 
 // Keys with special meaning. These will be delegated to the editor using
 // the execCommand() method
@@ -377,16 +387,38 @@ struct KeyPressEntry {
   const char* name;
 };
 
+// On Linux & OS X, the Windows virtual keycode constants are defined in the
+// WebCore namespace. In order not to import all of the WebCore namespace into
+// this file we need to wrap the keyDownEntries definition in it's own 
+// namespace.
+namespace key_lookup_table {
+  
+// On non-windows platforms, native key codes are translated into their Windows
+// counterparts.
+// We define the editor commands in terms of the Windows key codes on all 
+// platforms so we can reuse the dispatch table.
+using namespace WebCore;
+  
 static const KeyDownEntry keyDownEntries[] = {
-#if defined(OS_WIN)
   { VK_LEFT,   0,                  "MoveLeft"                                    },
   { VK_LEFT,   ShiftKey,           "MoveLeftAndModifySelection"                  },
+#if defined(OS_WIN)
   { VK_LEFT,   CtrlKey,            "MoveWordLeft"                                },
   { VK_LEFT,   CtrlKey | ShiftKey, "MoveWordLeftAndModifySelection"              },
+#elif defined(OS_MACOSX)
+  { VK_LEFT,   OptionKey,            "MoveWordLeft"                              },
+  { VK_LEFT,   OptionKey | ShiftKey, "MoveWordLeftAndModifySelection"            },
+#endif
   { VK_RIGHT,  0,                  "MoveRight"                                   },
   { VK_RIGHT,  ShiftKey,           "MoveRightAndModifySelection"                 },
+#if defined(OS_WIN)
   { VK_RIGHT,  CtrlKey,            "MoveWordRight"                               },
   { VK_RIGHT,  CtrlKey | ShiftKey, "MoveWordRightAndModifySelection"             },
+#elif defined(OS_MACOSX)
+  { VK_RIGHT,  OptionKey,            "MoveWordRight"                             },
+  { VK_RIGHT,  OptionKey | ShiftKey, "MoveWordRightAndModifySelection"           },
+#endif
+  
   { VK_UP,     0,                  "MoveUp"                                      },
   { VK_UP,     ShiftKey,           "MoveUpAndModifySelection"                    },
   { VK_PRIOR,  ShiftKey,           "MovePageUpAndModifySelection"                },
@@ -397,26 +429,50 @@ static const KeyDownEntry keyDownEntries[] = {
   { VK_NEXT,   0,                  "MovePageDown"                                },
   { VK_HOME,   0,                  "MoveToBeginningOfLine"                       },
   { VK_HOME,   ShiftKey,           "MoveToBeginningOfLineAndModifySelection"     },
+#if defined(OS_MACOSX)
+  { VK_LEFT,   CommandKey,            "MoveToBeginningOfLine"                    },
+  { VK_LEFT,   CommandKey | ShiftKey, "MoveToBeginningOfLineAndModifySelection"  },
+#endif
+  
+#if defined(OS_WIN)
   { VK_HOME,   CtrlKey,            "MoveToBeginningOfDocument"                   },
   { VK_HOME,   CtrlKey | ShiftKey, "MoveToBeginningOfDocumentAndModifySelection" },
+#elif defined(OS_MACOSX)
+  { VK_UP,   CommandKey,            "MoveToBeginningOfDocument"                   },
+  { VK_UP,   CommandKey | ShiftKey, "MoveToBeginningOfDocumentAndModifySelection" },  
+#endif
 	
   { VK_END,    0,                  "MoveToEndOfLine"                             },
   { VK_END,    ShiftKey,           "MoveToEndOfLineAndModifySelection"           },
+#if defined(OS_MACOSX)
+  { VK_RIGHT,    CommandKey,            "MoveToEndOfLine"                        },
+  { VK_RIGHT,    CommandKey | ShiftKey, "MoveToEndOfLineAndModifySelection"      },
+#endif
+  
+#if defined(OS_WIN)
   { VK_END,    CtrlKey,            "MoveToEndOfDocument"                         },
   { VK_END,    CtrlKey | ShiftKey, "MoveToEndOfDocumentAndModifySelection"       },
+#elif defined(OS_MACOSX)
+  { VK_DOWN,    CommandKey,            "MoveToEndOfDocument"                     },
+  { VK_DOWN,    CommandKey | ShiftKey, "MoveToEndOfDocumentAndModifySelection"   },
+#endif
 	
   { VK_BACK,   0,                  "DeleteBackward"                              },
   { VK_BACK,   ShiftKey,           "DeleteBackward"                              },
   { VK_DELETE, 0,                  "DeleteForward"                               },
+#if defined(OS_WIN)
   { VK_BACK,   CtrlKey,            "DeleteWordBackward"                          },
   { VK_DELETE, CtrlKey,            "DeleteWordForward"                           },
+#elif defined(OS_MACOSX)
+  { VK_BACK,   OptionKey,            "DeleteWordBackward"                        },
+  { VK_DELETE, OptionKey,            "DeleteWordForward"                         },
 #endif
 	   
   { 'B',       CtrlKey,            "ToggleBold"                                  },
   { 'I',       CtrlKey,            "ToggleItalic"                                },
   { 'U',       CtrlKey,            "ToggleUnderline"                             },
 
-#if defined(OS_WIN)
+
   { VK_ESCAPE, 0,                  "Cancel"                                      },
   { VK_OEM_PERIOD, CtrlKey,        "Cancel"                                      },
   { VK_TAB,    0,                  "InsertTab"                                   },
@@ -425,12 +481,12 @@ static const KeyDownEntry keyDownEntries[] = {
   { VK_RETURN, CtrlKey,            "InsertNewline"                               },
   { VK_RETURN, AltKey,             "InsertNewline"                               },
   { VK_RETURN, AltKey | ShiftKey,  "InsertNewline"                               },
-  { VK_RETURN, ShiftKey,           "InsertLineBreak"                             },	
+  { VK_RETURN, ShiftKey,           "InsertLineBreak"                             },
 
+#if defined(OS_WIN)
   { VK_INSERT, CtrlKey,            "Copy"                                        },	
   { VK_INSERT, ShiftKey,           "Paste"                                       },	
   { VK_DELETE, ShiftKey,           "Cut"                                         },	
-#endif
   { 'C',       CtrlKey,            "Copy"                                        },
   { 'V',       CtrlKey,            "Paste"                                       },
   { 'V',       CtrlKey | ShiftKey, "PasteAndMatchStyle"                          },
@@ -439,8 +495,19 @@ static const KeyDownEntry keyDownEntries[] = {
   { 'Z',       CtrlKey,            "Undo"                                        },
   { 'Z',       CtrlKey | ShiftKey, "Redo"                                        },
   { 'Y',       CtrlKey,            "Redo"                                        },
+#elif defined(OS_MACOSX)
+  { 'C',       CommandKey,            "Copy"                                     },
+  { 'V',       CommandKey,            "Paste"                                    },
+  { 'V',       CommandKey | ShiftKey, "PasteAndMatchStyle"                       },
+  { 'X',       CommandKey,            "Cut"                                      },
+  { 'A',       CommandKey,            "SelectAll"                                },
+  { 'Z',       CommandKey,            "Undo"                                     },
+  { 'Z',       CommandKey | ShiftKey, "Redo"                                     },  
+#endif
 };
 
+}  // namespace key_lookup_table
+  
 static const KeyPressEntry keyPressEntries[] = {
   { '\t',   0,                  "InsertTab"                                   },
   { '\t',   ShiftKey,           "InsertBacktab"                               },
@@ -464,6 +531,7 @@ const char* EditorClientImpl::interpretKeyEvent(
     keyDownCommandsMap = new HashMap<int, const char*>;
     keyPressCommandsMap = new HashMap<int, const char*>;
 
+    using key_lookup_table::keyDownEntries;
     for (unsigned i = 0; i < arraysize(keyDownEntries); i++) {
       keyDownCommandsMap->set(
         keyDownEntries[i].modifiers << 16 | keyDownEntries[i].virtualKey,
@@ -484,6 +552,8 @@ const char* EditorClientImpl::interpretKeyEvent(
     modifiers |= AltKey;
   if (keyEvent->ctrlKey())
     modifiers |= CtrlKey;
+  if (keyEvent->metaKey())
+    modifiers |= MetaKey;
 
   if (evt->type() == WebCore::EventNames::keydownEvent) {
     int mapKey = modifiers << 16 | evt->keyCode();
