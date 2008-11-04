36e72c9645d89a412cc192683220fb9b2c47cf17
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-04 21:53:24 +0000

    make it easier to type urls into testshell by adding http if there's no scheme Review URL: http://codereview.chromium.org/9138
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4672 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/mac/test_shell_webview.mm
+++ webkit/tools/test_shell/mac/test_shell_webview.mm
@@ -48,8 +48,6 @@
   CGContextSetRGBFillColor (context, 1, 0, 1, 1);
   CGContextFillRect(context, NSRectToCGRect(rect));
 
-  // uncomment when we stop passing the wrong kinds of GraphicsContext to
-  // WebCore
   if (shell_ && shell_->webView())
     shell_->webViewHost()->Paint();
 }
@@ -76,6 +74,12 @@
 
 - (IBAction)takeURLStringValueFrom:(NSTextField *)sender {
   NSString *url = [sender stringValue];
+  
+  // if it doesn't already have a prefix, add http. If we can't parse it, 
+  // just don't bother rather than making things worse.
+  NSURL* tempUrl = [NSURL URLWithString:url];
+  if (tempUrl && ![tempUrl scheme])
+    url = [@"http://" stringByAppendingString:url];
   shell_->LoadURL(UTF8ToWide([url UTF8String]).c_str());
 }
 
