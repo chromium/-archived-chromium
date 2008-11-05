17926475d02f4acb55e893a692ea907e622fcb42
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-05 20:52:29 +0000

    Mac build fixes following r4804.  Add files to the Mac project.  Add required #includes. Review URL: http://codereview.chromium.org/9195
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@4812 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/form_autocomplete_listener.cc
+++ webkit/glue/form_autocomplete_listener.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "config.h"
+
 #include "webkit/glue/form_autocomplete_listener.h"
 
 MSVC_PUSH_WARNING_LEVEL(0);
