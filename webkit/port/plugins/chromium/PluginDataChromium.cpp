f8b23ceb948bcfd0b16ca4d556271465f486190d
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-11 22:33:49 +0000

    Fix plugin data refreshing to work again.
    R=ojan
    
    Review URL: http://codereview.chromium.org/9767
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5216 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/plugins/chromium/PluginDataChromium.cpp
+++ webkit/port/plugins/chromium/PluginDataChromium.cpp
@@ -1,38 +1,49 @@
-// Copyright (c) 2008 The Chromium Authors. All rights reserved.  Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2008, Google Inc.
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// 
+//     * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+//     * Neither the name of Google Inc. nor the names of its
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "config.h"
 #include "PluginData.h"
 
 #include "PluginInfoStore.h"
 
-#undef LOG
-#include "webkit/glue/glue_util.h"
-#include "webkit/glue/webkit_glue.h"
-
 namespace WebCore {
-
-static bool refreshData = false;
 
 void PluginData::initPlugins()
 {
-    std::vector<WebPluginInfo> plugins;
-    if (!webkit_glue::GetPlugins(refreshData, &plugins))
-        return;
-    refreshData = false;
-
     PluginInfoStore c;
-    for (size_t i = 0; i < plugins.size(); ++i) {
-        PluginInfo* info = c.createPluginInfoForPluginAtIndex(i);
-        m_plugins.append(info);
-    }
+    for (size_t i = 0; i < c.pluginCount(); ++i)
+        m_plugins.append(c.createPluginInfoForPluginAtIndex(i));
 }
 
 void PluginData::refresh()
 {
-    // When next we initialize a PluginData, it'll be fresh.
-    refreshData = true;
+    refreshPlugins(true);
 }
 
 }
