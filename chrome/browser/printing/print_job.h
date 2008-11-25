7af1db5434d25f0e8ce751b7f296f891352ac946
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-25 14:45:39 +0000

    Reverting 5971. Review URL: http://codereview.chromium.org/12637
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@5974 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_job.h
+++ chrome/browser/printing/print_job.h
@@ -5,7 +5,6 @@
 #ifndef CHROME_BROWSER_PRINTING_PRINT_JOB_H_
 #define CHROME_BROWSER_PRINTING_PRINT_JOB_H_
 
-#include "base/message_loop.h"
 #include "base/ref_counted.h"
 #include "chrome/browser/printing/print_job_worker_owner.h"
 #include "chrome/common/notification_service.h"
@@ -33,8 +32,7 @@ class PrinterQuery;
 // runs in the UI thread.
 class PrintJob : public base::RefCountedThreadSafe<PrintJob>,
                  public NotificationObserver,
-                 public PrintJobWorkerOwner,
-                 public MessageLoop::DestructionObserver {
+                 public PrintJobWorkerOwner {
  public:
   // GetSettings() UI parameter.
   enum GetSettingsAskParam {
@@ -50,8 +48,6 @@ class PrintJob : public base::RefCountedThreadSafe<PrintJob>,
   PrintJob();
   virtual ~PrintJob();
 
-  // Grabs the ownership of the PrintJobWorker from another job, which is
-  // usually a PrinterQuery.
   void Initialize(PrintJobWorkerOwner* job, PrintedPagesSource* source);
 
   // NotificationObserver
@@ -72,9 +68,6 @@ class PrintJob : public base::RefCountedThreadSafe<PrintJob>,
   virtual MessageLoop* message_loop() { return ui_message_loop_; }
   virtual const PrintSettings& settings() const { return settings_; }
   virtual int cookie() const;
-
-  // DestructionObserver
-  virtual void WillDestroyCurrentMessageLoop();
 
   // Initializes the printing context. This can be done synchronously or not. It
   // is fine to call this function multiple times to reinitialize the settings.
