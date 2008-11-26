308e0f62a8bf13e24a3533ae6d78d92a4ef37fe8
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-26 00:16:04 +0000

    Implement the rest of ScreenInfo on Linux.
    Without this, we leak undefined data into V8 and cause lots of Valgrind
    errors.
    
    Review URL: http://codereview.chromium.org/12441
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6013 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/screen_info.h
+++ webkit/glue/screen_info.h
@@ -10,10 +10,27 @@
 namespace webkit_glue {
 
 struct ScreenInfo {
+  // The screen depth in bits per pixel
   int depth;
+  // The bits per colour component. This assumes that the colours are balanced
+  // equally.
   int depth_per_component;
+  // This can be true for black and white printers
   bool is_monochrome;
+  // This is set from the rcMonitor member of MONITORINFOEX, to whit:
+  //   "A RECT structure that specifies the display monitor rectangle,
+  //   expressed in virtual-screen coordinates. Note that if the monitor is not
+  //   the primary display monitor, some of the rectangle's coordinates may be
+  //   negative values."
   gfx::Rect rect;
+  // This is set from the rcWork member of MONITORINFOEX, to whit:
+  //   "A RECT structure that specifies the work area rectangle of the display
+  //   monitor that can be used by applications, expressed in virtual-screen
+  //   coordinates. Windows uses this rectangle to maximize an application on
+  //   the monitor. The rest of the area in rcMonitor contains system windows
+  //   such as the task bar and side bars. Note that if the monitor is not the
+  //   primary display monitor, some of the rectangle's coordinates may be
+  //   negative values".
   gfx::Rect available_rect;
 };
 
