b1f179a0a2a77e210a9007047cec5bc678e25ba2
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-26 08:38:02 +0000

    Clean up v8_binding.h by putting config.h at the top.
    R=fqian
    
    Review URL: http://codereview.chromium.org/12461
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@6031 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/v8_binding.h
+++ webkit/port/bindings/v8/v8_binding.h
@@ -5,29 +5,15 @@
 #ifndef V8_BINDING_H__
 #define V8_BINDING_H__
 
-#include "base/compiler_specific.h"
-#include "build/build_config.h"
+#include "config.h"
 
-#include <v8.h>
+#include "CString.h"
 #include "KURL.h"
-#include "PlatformString.h"
 #include "MathExtras.h"
+#include "PlatformString.h"
 #include "StringBuffer.h"
 
-// Suppress warnings in CString of converting size_t to unsigned int.
-// TODO(fqian): fix CString.h.
-MSVC_PUSH_WARNING_LEVEL(0);
-#include "CString.h"
-MSVC_POP_WARNING();
-
-#if defined(OS_LINUX)
-// Use the platform.h for linux.
-#include "unicode/plinux.h"
-#elif defined(OS_WIN) || defined(OS_MACOSX)
-// WebKit ships a hacked up version of one of the ICU header files, with all
-// options set for OSX.
-#include "platform.h"
-#endif
+#include <v8.h>
 
 namespace WebCore {
 
