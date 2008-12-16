918ba5758f52c6963b9ed1d4a829f1f42c82cfc2
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-16 20:26:08 +0000

    Fix Linux build failure caused by GCC 4.3/4.2 differences. Review URL: http://codereview.chromium.org/14481
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/gfx/favicon_size.h
+++ chrome/common/gfx/favicon_size.h
@@ -5,8 +5,14 @@
 #ifndef CHROME_COMMON_GFX_FAVICON_SIZE_H__
 #define CHROME_COMMON_GFX_FAVICON_SIZE_H__
 
+#include "build/build_config.h"
+
 // Size (along each axis) of the favicon.
 const int kFavIconSize = 16;
+
+#if !defined(OS_LINUX)
+// GCC 4.2 complains that this function is not used, and currently
+// it's not used by any file compiled on Linux. GCC 4.3 does not complain.
 
 // If the width or height is bigger than the favicon size, a new width/height
 // is calculated and returned in width/height that maintains the aspect
@@ -24,6 +30,8 @@ static void calc_favicon_target_size(int* width, int* height) {
     }
   }
 }
+
+#endif  // !defined(OS_LINUX)
 
 #endif // CHROME_COMMON_GFX_FAVICON_SIZE_H__
 
