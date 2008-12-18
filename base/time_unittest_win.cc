b7eebb303aaebab267d663cd5578c8a73f14eae0
mbelshe@google.com <mbelshe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-18 19:17:31 +0000

    Fix bug 3789: Turn off assert in automated tests for timer latency. This test isn't helping a lot, so removing it has no negative effect.  But the test fails on some VMs which run horribly slow.
    Leaving the test in place because it still prints out
    timer perf information, which can be useful for testing
    on laptops, low-end machines, etc.
    
    Review URL: http://codereview.chromium.org/14853
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7245 0039d316-1c4b-4281-b951-d872f2087c98

--- base/time_unittest_win.cc
+++ base/time_unittest_win.cc
@@ -190,7 +190,13 @@ TEST(TimeTicks, TimerPerformance) {
     for (int index = 0; index < kLoops; index++)
       cases[test_case].func();
     TimeTicks stop = TimeTicks::HighResNow();
-    EXPECT_LT((stop - start).InMilliseconds(), kMaxTime);
+    // Turning off the check for acceptible delays.  Without this check,
+    // the test really doesn't do much other than measure.  But the
+    // measurements are still useful for testing timers on various platforms.
+    // The reason to remove the check is because the tests run on many 
+    // buildbots, some of which are VMs.  These machines can run horribly
+    // slow, and there is really no value for checking against a max timer.
+    //EXPECT_LT((stop - start).InMilliseconds(), kMaxTime);
     printf("%s: %1.2fus per call\n", cases[test_case].description, 
       (stop - start).InMillisecondsF() * 1000 / kLoops);
     test_case++;
