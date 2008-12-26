70d0a92c242dac97b77ad276bee31f081dfb2886
evanm@google.com <evanm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-26 01:05:42 +0000

    Provide an unimplemented function to appease the dynamic linker.
    Review URL: http://codereview.chromium.org/16262
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7470 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/system_monitor_posix.cc
@@ -0,0 +1,16 @@
+// Copyright (c) 2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/system_monitor.h"
+
+#include "base/logging.h"
+
+namespace base {
+
+bool SystemMonitor::IsBatteryPower() {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+} // namespace base
