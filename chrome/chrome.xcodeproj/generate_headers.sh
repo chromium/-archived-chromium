d8af18afaf1838444f54387a2c66a9df95be91ed
pinkerton@google.com <pinkerton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-08 15:05:08 +0000

    check datestamps before blindly re-generating .h Review URL: http://codereview.chromium.org/17265
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@7731 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome.xcodeproj/generate_headers.sh
+++ chrome/chrome.xcodeproj/generate_headers.sh
@@ -9,10 +9,22 @@ GENERATED_DIR="${CONFIGURATION_TEMP_DIR}/generated"
 GRIT_DIR="${GENERATED_DIR}/grit"
 mkdir -p "${GRIT_DIR}"
 
-python "${PROJECT_DIR}/../tools/grit/grit.py" \
-    -i "${PROJECT_DIR}/app/generated_resources.grd" build \
-    -o "${GRIT_DIR}"
+# compare generated_resources.grd to generated_resources.h. If the .h is
+# older or doesn't exist, rebuild it.
+if [ "${GRIT_DIR}/generated_resources.h" -ot \
+     "${PROJECT_DIR}/app/generated_resources.grd" ]
+then
+  python "${PROJECT_DIR}/../tools/grit/grit.py" \
+      -i "${PROJECT_DIR}/app/generated_resources.grd" build \
+      -o "${GRIT_DIR}"
+fi
 
-python "${PROJECT_DIR}/../tools/grit/grit.py" \
-    -i "${PROJECT_DIR}/app/chromium_strings.grd" build \
-    -o "${GRIT_DIR}"
+# compare chromium_strings.grd to chromium_strings.h. If the .h is
+# older or doesn't exist, rebuild it
+if [ "${GRIT_DIR}/chromium_strings.h" -ot \
+     "${PROJECT_DIR}/app/chromium_strings.grd" ]
+then
+  python "${PROJECT_DIR}/../tools/grit/grit.py" \
+      -i "${PROJECT_DIR}/app/chromium_strings.grd" build \
+      -o "${GRIT_DIR}"
+fi
