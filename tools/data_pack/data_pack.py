2098febb97fad8defb1d439f7df5179ef94d4af8
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-15 01:15:48 +0000

    add a repack utility for combining .pak files generated by grit into a single .pak file.
    Review URL: http://codereview.chromium.org/18254
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8063 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/data_pack/data_pack.py
+++ tools/data_pack/data_pack.py
@@ -9,7 +9,32 @@ See base/pack_file* for details.
 
 import struct
 
-version = 1
+FILE_FORMAT_VERSION = 1
+HEADER_LENGTH = 2 * 4  # Two uint32s. (file version and number of entries)
+
+class WrongFileVersion(Exception):
+  pass
+
+def ReadDataPack(input_file):
+  """Reads a data pack file and returns a dictionary."""
+  data = open(input_file, "rb").read()
+  original_data = data
+
+  # Read the header.
+  version, num_entries = struct.unpack("<II", data[:HEADER_LENGTH])
+  if version != FILE_FORMAT_VERSION:
+    raise WrongFileVersion
+
+  resources = {}
+  # Read the index and data.
+  data = data[HEADER_LENGTH:]
+  kIndexEntrySize = 3 * 4  # Each entry is 3 uint32s.
+  for _ in range(num_entries):
+    id, offset, length = struct.unpack("<III", data[:kIndexEntrySize])
+    data = data[kIndexEntrySize:]
+    resources[id] = original_data[offset:offset + length]
+
+  return resources
 
 def WriteDataPack(resources, output_file):
   """Write a map of id=>data into output_file as a data pack."""
@@ -17,13 +42,12 @@ def WriteDataPack(resources, output_file):
   file = open(output_file, "wb")
 
   # Write file header.
-  file.write(struct.pack("<II", version, len(ids)))
-  header_length = 2 * 4             # Two uint32s.
+  file.write(struct.pack("<II", FILE_FORMAT_VERSION, len(ids)))
 
   index_length = len(ids) * 3 * 4   # Each entry is 3 uint32s.
 
   # Write index.
-  data_offset = header_length + index_length
+  data_offset = HEADER_LENGTH + index_length
   for id in ids:
     file.write(struct.pack("<III", id, data_offset, len(resources[id])))
     data_offset += len(resources[id])
@@ -35,8 +59,10 @@ def WriteDataPack(resources, output_file):
 def main():
   # Just write a simple file.
   data = { 1: "", 4: "this is id 4", 6: "this is id 6", 10: "" }
-  WriteDataPack(data, "datapack")
-  print "wrote datapack to current directory."
+  WriteDataPack(data, "datapack1.pak")
+  data2 = { 1000: "test", 5: "five" }
+  WriteDataPack(data2, "datapack2.pak")
+  print "wrote datapack1 and datapack2 to current directory."
 
 if __name__ == '__main__':
   main()
