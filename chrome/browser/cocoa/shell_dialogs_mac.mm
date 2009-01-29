35015ff21ef4d829cb31b3d0c6554a052af962af
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-29 18:41:13 +0000

    Add a stub shell dialogs to help us get linking faster. Review URL: http://codereview.chromium.org/19672
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8898 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/cocoa/shell_dialogs_mac.mm
@@ -0,0 +1,19 @@
+// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/shell_dialogs.h"
+
+#include "base/logging.h"
+
+SelectFileDialog* SelectFileDialog::Create(Listener* listener) {
+  NOTREACHED() << "Implement me.";
+  return NULL;
+}
+
+SelectFontDialog* SelectFontDialog::Create(Listener* listener) {
+  NOTREACHED() << "Implement me.";
+  return NULL;
+}
+
+
