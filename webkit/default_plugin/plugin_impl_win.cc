36af0ce89be7f94d9354753db3eeb1a4f8a4b95d
awalker@google.com <awalker@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-30 23:49:16 +0000

    Move the current default plugin implementation to being windows-specific and have plugin_impl.h include the correct version based on the platform.
    jam: primary
    evan: FYI
    
    
    Review URL: http://codereview.chromium.org/19727
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@8997 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/default_plugin/plugin_impl_win.cc
+++ webkit/default_plugin/plugin_impl_win.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "webkit/default_plugin/plugin_impl.h"
+#include "webkit/default_plugin/plugin_impl_win.h"
 
 #include <shellapi.h>
 
