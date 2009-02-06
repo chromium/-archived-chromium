4f70912360b8bb4a03c409caf52dc67f9fae38c8
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-06 03:32:12 +0000

    Move the about:ipc tab to a dialog. This patch is kind of a hack, the Windows specific code for the settings dialog and the table view are just copied from the old dialog. I tried to change as little as possible. The only significant change should be that I commented out saving the settings in the profile.
    This is a re-land of my previous patch which didn't ifdef the code based on the
    IPC logging preprocessor define.
    Previous review URL: http://codereview.chromium.org/19741
    Review URL: http://codereview.chromium.org/20033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9303 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/about_ipc_dialog.h
+++ chrome/browser/views/about_ipc_dialog.h
@@ -2,47 +2,57 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_VIEWS_ABOUT_NETWORK_DIALOG_H_
-#define CHROME_BROWSER_VIEWS_ABOUT_NETWORK_DIALOG_H_
+#ifndef CHROME_BROWSER_VIEWS_ABOUT_IPC_DIALOG_H_
+#define CHROME_BROWSER_VIEWS_ABOUT_IPC_DIALOG_H_
+
+#include <atlbase.h>
+#include <atlapp.h>
+#include <atlcrack.h>
+#include <atlctrls.h>
+#include <atlmisc.h>
 
 #include "base/singleton.h"
+#include "chrome/common/ipc_logging.h"
 #include "chrome/views/base_button.h"
 #include "chrome/views/dialog_delegate.h"
+#include "chrome/views/table_view.h"
 
+#if defined(OS_WIN) && defined(IPC_MESSAGE_LOG_ENABLED)
+
+class Profile;
 namespace views {
 class TextButton;
-class TextField;
+class HWNDView;
 }  // namespace views
 
-class AboutNetworkDialog : public views::DialogDelegate,
-                           public views::BaseButton::ButtonListener,
-                           public views::View {
+class AboutIPCDialog : public views::DialogDelegate,
+                       public views::BaseButton::ButtonListener,
+                       public IPC::Logging::Consumer,
+                       public views::View {
  public:
   // This dialog is a singleton. If the dialog is already opened, it won't do
   // anything, so you can just blindly call this function all you want.
   static void RunDialog();
 
-  virtual ~AboutNetworkDialog();
-
-  // Appends the given string to the dialog box. This is called by the job
-  // tracker (see the .cc file) when "stuff happens."
-  void AppendText(const std::wstring& text);
-
-  // Returns true if we're currently tracking network operations.
-  bool tracking() const { return tracking_; }
+  virtual ~AboutIPCDialog();
 
  private:
-  friend struct DefaultSingletonTraits<AboutNetworkDialog>;
+  friend struct DefaultSingletonTraits<AboutIPCDialog>;
 
-  AboutNetworkDialog();
+  AboutIPCDialog();
 
   // Sets up all UI controls for the dialog.
   void SetupControls();
 
+  // views::View overrides.
   virtual gfx::Size GetPreferredSize();
   virtual views::View* GetContentsView();
   virtual int GetDialogButtons() const;
   virtual std::wstring GetWindowTitle() const;
+  virtual void Layout();
+
+  // IPC::Logging::Consumer implementation.
+  virtual void Log(const IPC::LogData& data);
 
   // views::WindowDelegate (via view::DialogDelegate).
   virtual bool CanResize() const;
@@ -50,15 +60,19 @@ class AboutNetworkDialog : public views::DialogDelegate,
   // views::BaseButton::ButtonListener.
   virtual void ButtonPressed(views::BaseButton* button);
 
+  CListViewCtrl message_list_;
+
   views::TextButton* track_toggle_;
-  views::TextButton* show_button_;
   views::TextButton* clear_button_;
-  views::TextField* text_field_;
+  views::TextButton* filter_button_;
+  views::HWNDView* table_;
 
   // Set to true when we're tracking network status.
   bool tracking_;
 
-  DISALLOW_COPY_AND_ASSIGN(AboutNetworkDialog);
+  DISALLOW_COPY_AND_ASSIGN(AboutIPCDialog);
 };
 
-#endif  // CHROME_BROWSER_VIEWS_ABOUT_NETWORK_DIALOG_H_
+#endif  // OS_WIN && IPC_MESSAGE_LOG_ENABLED
+
+#endif  // CHROME_BROWSER_VIEWS_ABOUT_IPC_DIALOG_H_
