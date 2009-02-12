92c418ace2bca3e71e0ed75566d5ab675e0b2d64
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-12 00:46:53 +0000

    Fix totally broken include set.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@9628 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_thread.cc
+++ chrome/renderer/render_thread.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
+
 #if defined(OS_WIN)
 #include <windows.h>
 #include <objbase.h>
