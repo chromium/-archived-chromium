41aa4d799ee1a5c6a618c622edafffa820e6fb09
ericroman@google.com <ericroman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-19 08:51:45 +0000

    Fix header order.
    Review URL: http://codereview.chromium.org/20487
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10013 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_server.cc
+++ net/proxy/proxy_server.cc
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <algorithm>
-
 #include "net/proxy/proxy_server.h"
+
+#include <algorithm>
 
 #include "base/string_tokenizer.h"
 #include "base/string_util.h"
