1b0ee130d81aa3205d70c1c8957d6ea40184ca58
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-20 06:07:02 +0000

    Fix release build break
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10081 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/child_thread.cc
+++ chrome/common/child_thread.cc
@@ -5,6 +5,7 @@
 #include "chrome/common/child_thread.h"
 
 #include "base/command_line.h"
+#include "base/string_util.h"
 #include "chrome/common/child_process.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/ipc_logging.h"
