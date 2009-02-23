45aa8a88f0b0e5f76d22876a2adc5c9d59bb2879
mmoss@chromium.org <mmoss@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-23 23:56:40 +0000

    Fix Linux shared build breakage due to undefined references.
    This compiles an empty file so we can generate the libplugin target needed by
    other targets. This whole file does compile (see r9934), but it doesn't link
    because of undefined refs to files which aren't compiling yet (e.g.
    npobject_proxy stuff). But we need some sources to build an empty lib,
    otherwise scons fails to link in skia (when linking with no source files, the
    -L flag is left off the linker command, so libs in the build directory can't be
    found).
    
    Review URL: http://codereview.chromium.org/27019
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10227 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/plugin/npobject_util.cc
+++ chrome/plugin/npobject_util.cc
@@ -2,12 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(port) Just compile an empty file on posix so we can generate the
+// libplugin target needed by other targets. This whole file does compile (see
+// r9934), but it doesn't link because of undefined refs to files which aren't
+// compiling yet (e.g. npobject_proxy stuff).
+#if defined(OS_WIN)
 #include "chrome/plugin/npobject_util.h"
 
-#if defined(OS_WIN)
 #include "chrome/common/plugin_messages.h"
 #include "chrome/common/win_util.h"
-#endif
 
 #include "chrome/plugin/npobject_proxy.h"
 #include "chrome/plugin/plugin_channel_base.h"
@@ -248,4 +251,5 @@ void CreateNPVariant(const NPVariant_Param& param,
       NOTREACHED();
   }
 }
+#endif
 #endif
