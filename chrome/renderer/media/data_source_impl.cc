b8a66a5921ceb5c2daa75af78f5b9eb0b34dc26b
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-24 01:48:02 +0000

    Fix missing include in r10237 TBR=?
    Review URL: http://codereview.chromium.org/28059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10243 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/media/data_source_impl.cc
+++ chrome/renderer/media/data_source_impl.cc
@@ -2,6 +2,7 @@
 // source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
+#include "base/compiler_specific.h"
 #include "base/message_loop.h"
 #include "chrome/renderer/media/data_source_impl.h"
 #include "chrome/renderer/render_view.h"
