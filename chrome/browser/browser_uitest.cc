1807a5b05e2091d677ed969b55d1675d1776fd94
kuchhal@chromium.org <kuchhal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-26 17:25:37 +0000

    Disabling ThirtyFourTabs test.
    TBR=jam
    BUG=8119
    
    Review URL: http://codereview.chromium.org/28190
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10475 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_uitest.cc
+++ chrome/browser/browser_uitest.cc
@@ -89,7 +89,8 @@ TEST_F(BrowserTest, Title) {
 // exact number of processes depends on the amount of memory. Previously we
 // had a hard limit of 31 processes and this test is mainly directed at
 // verifying that we don't crash when we pass this limit.
-TEST_F(BrowserTest, ThirtyFourTabs) {
+// Disabling this test http://code.google.com/p/chromium/issues/detail?id=8119
+TEST_F(BrowserTest, DISABLED_ThirtyFourTabs) {
   std::wstring test_file = test_data_directory_;
   file_util::AppendToPath(&test_file, L"title2.html");
   GURL url(net::FilePathToFileURL(test_file));
@@ -281,4 +282,3 @@ TEST_F(VisibleBrowserTest, WindowOpenClose) {
   if (i == 10)
     FAIL() << "failed to get error page title";
 }
-
