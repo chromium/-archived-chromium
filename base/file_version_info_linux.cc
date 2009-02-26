93d8ba79a41784b08f4a60f56b6188e1c116134d
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-26 23:17:51 +0000

    Convert FileVersionInfo NOTIMPLEMENTED()s into a bug.
    Review URL: http://codereview.chromium.org/27232
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@10518 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_version_info_linux.cc
+++ base/file_version_info_linux.cc
@@ -8,14 +8,12 @@
 
 #include "base/logging.h"
 
-// TODO(port): Replace stubs with real implementations. We need a non-NULL
-// FileVersionInfo object that reads info about the current binary. This is
-// relatively easy to do under Windows, as there's some win32 API functions
-// that return that information and Microsoft encourages developers to fill out
-// that standard information block.
+// TODO(port): This whole file is stubbed. We can't return NULL
+// because that is used as a catastrophic error code where the file
+// doesn't exist or can't be opened.
 //
-// We can't return NULL because that is used as a catastrophic error code where
-// the file doesn't exist or can't be opened.
+// See http://code.google.com/p/chromium/issues/detail?id=8132 for a discussion
+// on what we should do with this module.
 
 FileVersionInfo::FileVersionInfo() {}
 
@@ -23,87 +21,69 @@ FileVersionInfo::~FileVersionInfo() {}
 
 // static
 FileVersionInfo* FileVersionInfo::CreateFileVersionInfoForCurrentModule() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return new FileVersionInfo();
 }
 
 std::wstring FileVersionInfo::company_name() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::company_short_name() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::product_name() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::product_short_name() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::internal_name() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::product_version() {
-  // When un-stubbing, implementation in file_version_info.cc should be ok.
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"0.1.2.3.4.5.6-lie";
 }
 
 std::wstring FileVersionInfo::private_build() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::special_build() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::comments() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::original_filename() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::file_description() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::file_version() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"0.1.2.3.4.5.6-lie";
 }
 
 std::wstring FileVersionInfo::legal_copyright() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::legal_trademarks() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"";
 }
 
 std::wstring FileVersionInfo::last_change() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return L"Last Thursday";
 }
 
 bool FileVersionInfo::is_official_build() {
-  NOTIMPLEMENTED() << "The current FileVersionInfo is a lie.";
   return false;
 }
