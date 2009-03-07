13efadcfff3a56e46b4737f72cbadcbf893b24ac
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-07 08:30:29 +0000

    Allow clicks to windows disabled by modal dialogs to bring the entire modal stack to the front. Apparently you need to let DefWindowProc handle WM_SETCURSOR unless you want clicks to background disabled windows to get lost.
    http://crbug.com/8437
    
    Review URL: http://codereview.chromium.org/39308
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/window.cc
+++ chrome/views/window.cc
@@ -789,6 +789,19 @@ LRESULT Window::OnNCUAHDrawFrame(UINT msg, WPARAM w_param,
 }
 
 LRESULT Window::OnSetCursor(HWND window, UINT hittest_code, UINT message) {
+  // If the window is disabled, it's because we're showing a modal dialog box.
+  // We need to let DefWindowProc handle the message. That's because
+  // DefWindowProc for WM_SETCURSOR with message = some kind of mouse button
+  // down message sends the top level window a WM_ACTIVATEAPP message, which we
+  // otherwise wouldn't get. The symptom of not doing this is that if the user
+  // has a window in the background with a modal dialog open, they can't click
+  // on the disabled background window to bring the entire stack to the front.
+  // This is annoying because they then have to move all the foreground windows
+  // out of the way to be able to activate said window. I love how on Windows,
+  // the answer isn't always logical.
+  if (!IsWindowEnabled(GetHWND()))
+    return WidgetWin::OnSetCursor(window, hittest_code, message);
+
   int index = RC_NORMAL;
   switch (hittest_code) {
     case HTTOP:
