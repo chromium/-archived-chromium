80c0ed34355184610128d233a9aabfb6dd9d3d6b
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-10 12:47:14 +0000

    Add a copyright header to location_bar_view_gtk.cc.
    This is a new file committed in r11323, adding a proper copyright header.
    
    Review URL: http://codereview.chromium.org/43013
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11324 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/location_bar_view_gtk.cc
+++ chrome/browser/gtk/location_bar_view_gtk.cc
@@ -1,3 +1,7 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "chrome/browser/gtk/location_bar_view_gtk.h"
 
 #include <string>
