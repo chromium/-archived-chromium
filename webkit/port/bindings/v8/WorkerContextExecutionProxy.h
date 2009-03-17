b281bdbf108d88bc66a46d7aa3af426051406e1f
dimich@google.com <dimich@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-17 23:49:22 +0000

    Pre-upstream cleaning of files - changing them to follow WebKit coding style. Review URL: http://codereview.chromium.org/42308
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11935 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/v8/WorkerContextExecutionProxy.h
+++ webkit/port/bindings/v8/WorkerContextExecutionProxy.h
@@ -1,120 +1,112 @@
-// Copyright (c) 2009, Google Inc.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-//     * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//     * Neither the name of Google Inc. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Copyright (C) 009 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#ifndef WorkerContextExecutionProxy_h_
-#define WorkerContextExecutionProxy_h_
+
+#ifndef WorkerContextExecutionProxy_h
+#define WorkerContextExecutionProxy_h
 
 #if ENABLE(WORKERS)
 
 #include <list>
-#include <wtf/HashSet.h>
 #include "v8.h"
 #include "v8_index.h"
+#include <wtf/HashSet.h>
 
 namespace WebCore {
 
-class Event;
-class EventTarget;
-class V8EventListener;
-class WorkerContext;
+    class Event;
+    class EventTarget;
+    class V8EventListener;
+    class WorkerContext;
 
-class WorkerContextExecutionProxy {
- public:
-  WorkerContextExecutionProxy(WorkerContext*);
-  ~WorkerContextExecutionProxy();
+    class WorkerContextExecutionProxy {
+    public:
+        WorkerContextExecutionProxy(WorkerContext*);
+        ~WorkerContextExecutionProxy();
 
-  // Returns a local handle of the context.
-  v8::Local<v8::Context> GetContext() {
-    return v8::Local<v8::Context>::New(m_context);
-  }
+        // Returns a local handle of the context.
+        v8::Local<v8::Context> GetContext() { return v8::Local<v8::Context>::New(m_context); }
 
-  // Returns the dom constructor function for the given node type.
-  v8::Local<v8::Function> GetConstructor(V8ClassIndex::V8WrapperType t);
+        // Returns the dom constructor function for the given node type.
+        v8::Local<v8::Function> GetConstructor(V8ClassIndex::V8WrapperType);
 
-  // Finds or creates an event listener;
-  PassRefPtr<V8EventListener> FindOrCreateEventListener(
-      v8::Local<v8::Value> listener, bool is_inline, bool find_only);
+        // Finds or creates an event listener;
+        PassRefPtr<V8EventListener> FindOrCreateEventListener(v8::Local<v8::Value> listener, bool isInline, bool findOnly);
 
-  // Removes an event listener;
-  void RemoveEventListener(V8EventListener* listener);
+        // Removes an event listener;
+        void RemoveEventListener(V8EventListener*);
 
-  // Track the event so that we can detach it from the JS wrapper when a worker
-  // terminates. This is needed because we need to be able to dispose these
-  // events and releases references to their event targets: WorkerContext.
-  void TrackEvent(Event* event);
+        // Track the event so that we can detach it from the JS wrapper when a worker
+        // terminates. This is needed because we need to be able to dispose these
+        // events and releases references to their event targets: WorkerContext.
+        void TrackEvent(Event*);
 
-  // Evaluate a script file in the current execution environment.
-  v8::Local<v8::Value> Evaluate(const String& str,
-                                const String& file_name,
-                                int base_line);
+        // Evaluate a script file in the current execution environment.
+        v8::Local<v8::Value> evaluate(const String& str, const String& fileName, int baseLine);
 
-  // Returns WorkerContext object.
-  WorkerContext* worker_context() { return m_workerContext; }
+        // Returns WorkerContext object.
+        WorkerContext* workerContext() { return m_workerContext; }
 
-  // Returns WorkerContextExecutionProxy object of the currently executing
-  // context.
-  static WorkerContextExecutionProxy* retrieve();
+        // Returns WorkerContextExecutionProxy object of the currently executing context.
+        static WorkerContextExecutionProxy* retrieve();
 
-  // Returns the JS wrapper of object.
-  static v8::Handle<v8::Value> ToV8Object(V8ClassIndex::V8WrapperType type,
-                                          void* imp);
-  static v8::Handle<v8::Value> EventToV8Object(Event* event);
-  static v8::Handle<v8::Value> EventTargetToV8Object(EventTarget* target);
-  static v8::Handle<v8::Value> WorkerContextToV8Object(WorkerContext* wc);
+        // Returns the JS wrapper of object.
+        static v8::Handle<v8::Value> ToV8Object(V8ClassIndex::V8WrapperType type, void* impl);
+        static v8::Handle<v8::Value> EventToV8Object(Event* event);
+        static v8::Handle<v8::Value> EventTargetToV8Object(EventTarget* target);
+        static v8::Handle<v8::Value> WorkerContextToV8Object(WorkerContext* wc);
 
- private:
-  void InitContextIfNeeded();
-  void Dispose();
+    private:
+        void initContextIfNeeded();
+        void dispose();
 
-  // Run an already compiled script.
-  v8::Local<v8::Value> RunScript(v8::Handle<v8::Script> script);
+        // Run an already compiled script.
+        v8::Local<v8::Value> runScript(v8::Handle<v8::Script>);
 
-  static v8::Local<v8::Object> InstantiateV8Object(
-      V8ClassIndex::V8WrapperType desc_type,
-      V8ClassIndex::V8WrapperType cptr_type,
-      void* impl);
+        static v8::Local<v8::Object> instantiateV8Object(V8ClassIndex::V8WrapperType descType, V8ClassIndex::V8WrapperType cptrType, void* impl);
 
-  static bool ForgetV8EventObject(Event* event);
+        static bool forgetV8EventObject(Event*);
 
-  WorkerContext* m_workerContext;
-  v8::Persistent<v8::Context> m_context;
-  int m_recursion;
+        WorkerContext* m_workerContext;
+        v8::Persistent<v8::Context> m_context;
+        int m_recursion;
 
-  typedef std::list<V8EventListener*> EventListenerList;
-  EventListenerList m_listeners;
+        typedef std::list<V8EventListener*> EventListenerList;
+        EventListenerList m_listeners;
 
-  typedef HashSet<Event*> EventSet;
-  EventSet m_events;
-};
+        typedef HashSet<Event*> EventSet;
+        EventSet m_events;
+    };
 
 } // namespace WebCore
 
 #endif // ENABLE(WORKERS)
 
-#endif // WorkerContextExecutionProxy_h_
+#endif // WorkerContextExecutionProxy_h
--- webkit/port/bindings/v8/WorkerScriptController.cpp
+++ webkit/port/bindings/v8/WorkerScriptController.cpp
@@ -1,37 +1,40 @@
-// Copyright (c) 2008, Google Inc.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-//     * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//     * Neither the name of Google Inc. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Copyright (C) 2009 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "config.h"
 
 #if ENABLE(WORKERS)
 
 #include "WorkerScriptController.h"
+
+#include "v8.h"
 
 #include "ScriptSourceCode.h"
 #include "ScriptValue.h"
@@ -62,14 +65,8 @@ ScriptValue WorkerScriptController::evaluate(const ScriptSourceCode& sourceCode)
             return ScriptValue();
     }
 
-    v8::Local<v8::Value> result =
-        m_proxy->Evaluate(sourceCode.source(),
-                          sourceCode.url().string(),
-                          sourceCode.startLine() - 1);
-
-    m_workerContext->thread()->workerObjectProxy()->
-        reportPendingActivity(m_workerContext->hasPendingActivity());
-
+    v8::Local<v8::Value> result = m_proxy->evaluate(sourceCode.source(), sourceCode.url().string(), sourceCode.startLine() - 1);
+    m_workerContext->thread()->workerObjectProxy()->reportPendingActivity(m_workerContext->hasPendingActivity());
     return ScriptValue();
 }
 
@@ -88,7 +85,6 @@ void WorkerScriptController::forbidExecution()
 
 void WorkerScriptController::setException(ScriptValue exception)
 {
-    // FIXME(jianli): to be implemented.
     notImplemented();
 }
 
