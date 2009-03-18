7f971d1a43e801965361f34834c061a50f3cdd1d
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-18 13:12:46 +0000

    Initialize module_handle_ in IATPatch's constructor.
    The uninitialized value wasn't used since it was guarded by other members, but it is still better to initialize the member explicitly.
    
    Review URL: http://codereview.chromium.org/48130
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@11959 0039d316-1c4b-4281-b951-d872f2087c98

--- base/iat_patch.cc
+++ base/iat_patch.cc
@@ -169,7 +169,8 @@ DWORD ModifyCode(void* old_code, void* new_code, int length) {
 }
 
 IATPatchFunction::IATPatchFunction()
-    : original_function_(NULL),
+    : module_handle_(NULL),
+      original_function_(NULL),
       iat_thunk_(NULL),
       intercept_function_(NULL) {
 }
