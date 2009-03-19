252e8f5c777b7e67f8a178dde041dd2f02ce30ab
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-19 22:46:04 +0000

    Linux: fix crash when getting the screen info for a NULL window.
    Since we still have the hack where we pass raw pointers into the
    renderer, we should catch the NULL case and return something sane.
    
    Additionally, while I'm at it, always tell WebKit that it has a 32-bit
    screen. Since it paints via IPC messages, it doesn't need to know what
    the actual screen is using; we handle all that in the BackingStore.
    
    Review URL: http://codereview.chromium.org/50030
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12167 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkit_glue_gtk.cc
+++ webkit/glue/webkit_glue_gtk.cc
@@ -11,21 +11,21 @@
 namespace webkit_glue {
 
 ScreenInfo GetScreenInfoHelper(gfx::NativeView window) {
-  GdkScreen* screen = gtk_widget_get_screen(GTK_WIDGET(window));
-  GdkVisual* visual = gdk_screen_get_system_visual(screen);
-
   ScreenInfo results;
+  results.depth = 32;
+  results.depth_per_component = 8;
+  results.is_monochrome = false;
+
+  if (!window)
+    return results;
+
+  GdkScreen* screen = gtk_widget_get_screen(GTK_WIDGET(window));
   results.rect.SetRect(0, 0, gdk_screen_get_width(screen),
                        gdk_screen_get_height(screen));
   // I don't know of a way to query the "maximise" size of the window (e.g.
   // screen size less sidebars etc) since this is something which only the
   // window manager knows.
   results.available_rect = results.rect;
-
-  results.depth = visual->depth;
-  results.depth_per_component = visual->bits_per_rgb;
-
-  results.is_monochrome = false;
 
   return results;
 }
