a2477a258151ab8363ce428e34d4961dc2efb3d1
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-19 15:08:29 +0000

    Add some UBC eviction code after talking to Amit (and it doesn't even require a kext)  :) Review URL: http://codereview.chromium.org/42391
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12090 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test_file_util_mac.cc
+++ base/test_file_util_mac.cc
@@ -4,14 +4,32 @@
 
 #include "base/test_file_util.h"
 
+#include <sys/mman.h>
+#include <errno.h>
 #include "base/logging.h"
+#include "base/file_util.h"
 
 namespace file_util {
 
 bool EvictFileFromSystemCache(const FilePath& file) {
-  // There is no way that we can think of to dump something from the UBC.  You
-  // can add something so when you open it, it doesn't go in, but that won't
-  // work here.
+  // There aren't any really direct ways to purge a file from the UBC.  From
+  // talking with Amit Singh, the safest is to mmap the file with MAP_FILE (the
+  // default) + MAP_SHARED, then do an msync to invalidate the memory.  The next
+  // open should then have to load the file from disk.
+
+  file_util::MemoryMappedFile mapped_file;
+  if (!mapped_file.Initialize(file)) {
+    DLOG(WARNING) << "failed to memory map " << file.value();
+    return false;
+  }
+  
+  if (msync(const_cast<uint8*>(mapped_file.data()), mapped_file.length(),
+            MS_INVALIDATE) != 0) {
+    DLOG(WARNING) << "failed to invalidate memory map of " << file.value() 
+        << ", errno: " << errno;
+    return false;
+  }
+  
   return true;
 }
 
