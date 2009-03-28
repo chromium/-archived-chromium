a579f9e4a175ed960832dc3cf332a1c8a4898357
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-28 01:45:58 +0000

    Facilitate SDCH content decoding after a proxy removes ALL content info
    Some proxies remove all content-encoding information when they see
    that SDCH encoding was done.  We had a premature optimization that
    only called for doing "fixups" of content encoding IF there was
    some content encoding.  We need to do the fixups as needed even
    if the HTTP headers didn't supply any encodings (such as when
    a proxy or anti-virus software strips all encodings!)
    
    bug=9151
    
    r=huanr,kmixter,openvcdiff
    Review URL: http://codereview.chromium.org/56034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12731 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -205,9 +205,12 @@ bool URLRequestHttpJob::GetContentEncodings(
     encoding_types->push_back(Filter::ConvertEncodingToType(encoding_type));
   }
 
-  if (!encoding_types->empty()) {
-    Filter::FixupEncodingTypes(*this, encoding_types);
-  }
+  // Even if encoding types are empty, there is a chance that we need to add
+  // some decoding, as some proxies strip encoding completely. In such cases,
+  // we may need to add (for example) SDCH filtering (when the context suggests
+  // it is appropriate).
+  Filter::FixupEncodingTypes(*this, encoding_types);
+
   return !encoding_types->empty();
 }
 
