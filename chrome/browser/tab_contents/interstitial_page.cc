ca7f85e6d74186e74957ad1346ce1c1030fab9e1
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-28 01:47:58 +0000

    A crash report from the field reports that we are removing a SafeBrowsingBlockingPage from a global map that is not to be found causing a stl exception to be thrown. I am not sure what the cause is yet, my investigations results are in the bug comment. This is a work-around the crasher while I keep investigating.
    BUG=9442
    TEST=Access a page which contains sub-resource malware.
    Review URL: http://codereview.chromium.org/49049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12732 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/interstitial_page.cc
+++ chrome/browser/tab_contents/interstitial_page.cc
@@ -126,8 +126,11 @@ InterstitialPage::InterstitialPage(WebContents* tab,
 
 InterstitialPage::~InterstitialPage() {
   InterstitialPageMap::iterator iter = tab_to_interstitial_page_->find(tab_);
-  DCHECK(iter != tab_to_interstitial_page_->end());
-  tab_to_interstitial_page_->erase(iter);
+  DCHECK(iter != tab_to_interstitial_page_->end()) <<
+      "InterstitialPage missing from map. Please add a comment to the bug "
+      "http://crbug.com/9442 with the URL you were visiting";
+  if (iter != tab_to_interstitial_page_->end())
+    tab_to_interstitial_page_->erase(iter);
   DCHECK(!render_view_host_);
 }
 
