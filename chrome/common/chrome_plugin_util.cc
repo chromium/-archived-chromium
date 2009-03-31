1e7dcded7252c53b1f20816adec080d6cfa06e45
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-31 03:40:07 +0000

    Make app installation work when --user-data-dir is specified.
    BUG=9546
    Review URL: http://codereview.chromium.org/58004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@12835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_plugin_util.cc
+++ chrome/common/chrome_plugin_util.cc
@@ -137,7 +137,7 @@ CPError CPB_GetCommandLineArgumentsCommon(const char* url,
     if (file_util::AbsolutePath(&user_data_dir) &&
         file_util::PathExists(user_data_dir)) {
       arguments_w += std::wstring(L"--") + switches::kUserDataDir +
-                     L'=' + user_data_dir;
+                     L"=\"" + user_data_dir + L"\" ";
     }
   }
 
