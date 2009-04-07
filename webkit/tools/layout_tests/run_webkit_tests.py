3a8ea9fc2ae14188204c4d600d7f8193cce3af34
ojan@chromium.org <ojan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-07 06:02:46 +0000

    Ensure that the http tests are always run first since they take so long. Review URL: http://codereview.chromium.org/62090
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13230 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/layout_tests/run_webkit_tests.py
+++ webkit/tools/layout_tests/run_webkit_tests.py
@@ -354,8 +354,17 @@ class TestRunner:
       # Keep the tests in alphabetical order.
       # TODO: Remove once tests are fixed so they can be run in any order.
       test_list.reverse()
-      test_lists.append((directory, test_list))
+      test_list_tuple = (directory, test_list)
+      if directory == 'LayoutTests' + os.sep + 'http':
+        http_tests = test_list_tuple
+      else:
+        test_lists.append(test_list_tuple)
     test_lists.sort(lambda a, b: cmp(len(b), len(a)))
+
+    # Put the http tests first. There are only a couple hundred of them, but
+    # each http test takes a very long time to run, so sorting by the number
+    # of tests doesn't accurately capture how long they take to run.
+    test_lists.insert(0, http_tests)
 
     filename_queue = Queue.Queue()
     for item in test_lists:
