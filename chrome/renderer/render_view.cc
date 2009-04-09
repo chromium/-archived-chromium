952405ab55060031ba55f3879bb3ac0950bc15c2
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-09 14:39:32 +0000

    DevTools: Cache resource metainfo even when debugger agent is not attached. - Cache 200 resources - Hide agent functionality behind the flag Review URL: http://codereview.chromium.org/62183
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -299,13 +299,19 @@ void RenderView::Init(gfx::NativeViewId parent_hwnd,
     decrement_shared_popup_at_destruction_ = false;
   }
 
-  devtools_agent_.reset(new DevToolsAgent(routing_id, this));
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+
+  bool dev_tools_enabled = command_line.HasSwitch(
+      switches::kEnableOutOfProcessDevTools);
+  if (dev_tools_enabled)
+    devtools_agent_.reset(new DevToolsAgent(routing_id, this));
 
   webwidget_ = WebView::Create(this, webkit_prefs);
 
-  devtools_agent_filter_ = new DevToolsAgentFilter(
-      webview()->GetWebDevToolsAgent(),
-      routing_id);
+  if (dev_tools_enabled)
+    devtools_agent_filter_ = new DevToolsAgentFilter(
+        webview()->GetWebDevToolsAgent(),
+        routing_id);
 
 #if defined(OS_LINUX)
   // We have to enable ourselves as the editor delegate on linux so we can copy
@@ -334,7 +340,6 @@ void RenderView::Init(gfx::NativeViewId parent_hwnd,
   host_window_ = parent_hwnd;
   modal_dialog_event_.reset(modal_dialog_event);
 
-  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   if (command_line.HasSwitch(switches::kDomAutomationController))
     enabled_bindings_ |= BindingsPolicy::DOM_AUTOMATION;
   disable_popup_blocking_ =
@@ -342,7 +347,8 @@ void RenderView::Init(gfx::NativeViewId parent_hwnd,
 
   debug_message_handler_ = new DebugMessageHandler(this);
   render_thread_->AddFilter(debug_message_handler_);
-  render_thread_->AddFilter(devtools_agent_filter_);
+  if (dev_tools_enabled)
+    render_thread_->AddFilter(devtools_agent_filter_);
 }
 
 void RenderView::OnMessageReceived(const IPC::Message& message) {
@@ -353,7 +359,7 @@ void RenderView::OnMessageReceived(const IPC::Message& message) {
   // If this is developer tools renderer intercept tools messages first.
   if (devtools_client_.get() && devtools_client_->OnMessageReceived(message))
     return;
-  if (devtools_agent_->OnMessageReceived(message))
+  if (devtools_agent_.get() && devtools_agent_->OnMessageReceived(message))
     return;
 
   IPC_BEGIN_MESSAGE_MAP(RenderView, message)
