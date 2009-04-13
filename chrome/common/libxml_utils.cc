ffe25fe685f76fc78accd3f4dce6d926f29a5e27
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-13 22:17:02 +0000

    Fix automated_ui_testing for XMLWriter
    There was an error in InitXMLReader where it tries to delete the bufffer_
    member,
    setting it to null will make it work as mulhern stated.
    
       \src\chrome\Debug>automated_ui_tests.exe
       Started crash_service.exe so you know if a test crashes!
       [==========] Running 1 test from 1 test case.
       [----------] Global test environment set-up.
       [----------] 1 test from AutomatedUITest
       [ RUN      ] AutomatedUITest.TheOneAndOnlyTest
       .\automated_ui_tests.cc(167): error: Value of: InitXMLReader()
       Actual: false
       Expected: true
       Error initializing XMLReader
    
    Then it crashes.
    
    TEST=Ran the test again, doesn't crash.
    
    BUG=9625 http://crbug.com/9625
    
    Patch by Mohamed Mansour <m0.interactive@gmail.com> in
    http://codereview.chromium.org/63001
    
    Review URL: http://codereview.chromium.org/67094
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13616 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/libxml_utils.cc
+++ chrome/common/libxml_utils.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -120,7 +120,8 @@ bool XmlReader::SkipToElement() {
 // XmlWriter functions
 
 XmlWriter::XmlWriter() :
-    writer_(NULL) {}
+    writer_(NULL),
+    buffer_(NULL) {}
 
 XmlWriter::~XmlWriter() {
   if (writer_)
