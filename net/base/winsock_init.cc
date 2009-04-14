7e690fea4f5eaf43246c755e695bab3487a059a6
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-14 18:02:56 +0000

    Assert that the WinSock DLL supports version 2.2.
    R=cpu
    Review URL: http://codereview.chromium.org/67111
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@13672 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/winsock_init.cc
+++ net/base/winsock_init.cc
@@ -6,6 +6,7 @@
 
 #include "net/base/winsock_init.h"
 
+#include "base/logging.h"
 #include "base/singleton.h"
 
 namespace {
@@ -13,17 +14,21 @@ namespace {
 class WinsockInitSingleton {
  public:
   WinsockInitSingleton() : did_init_(false) {
-    WORD winsock_ver = MAKEWORD(2,2);
+    WORD winsock_ver = MAKEWORD(2, 2);
     WSAData wsa_data;
     did_init_ = (WSAStartup(winsock_ver, &wsa_data) == 0);
+    if (did_init_) {
+      DCHECK(wsa_data.wVersion == winsock_ver);
 
-    // The first time WSAGetLastError is called, the delay load helper will
-    // resolve the address with GetProcAddress and fixup the import.  If a third
-    // party application hooks system functions without correctly restoring the
-    // error code, it is possible that the error code will be overwritten during
-    // delay load resolution.  The result of the first call may be incorrect, so
-    // make sure the function is bound and future results will be correct.
-    WSAGetLastError();
+      // The first time WSAGetLastError is called, the delay load helper will
+      // resolve the address with GetProcAddress and fixup the import.  If a
+      // third party application hooks system functions without correctly
+      // restoring the error code, it is possible that the error code will be
+      // overwritten during delay load resolution.  The result of the first
+      // call may be incorrect, so make sure the function is bound and future
+      // results will be correct.
+      WSAGetLastError();
+    }
   }
 
   ~WinsockInitSingleton() {
