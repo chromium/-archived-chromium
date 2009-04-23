8083a1c45114b4a9800759ad1b3bc4fa98cedfb1
robertshield@google.com <robertshield@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-23 18:11:51 +0000

    Disabling flaky StatsTableTest.MultipleThreads on Linux too.
    Fails here for example: http://build.chromium.org/buildbot/waterfall/builders/Modules%20Linux%20(dbg)/builds/7281/steps/base_unittests/logs/stdio
    
    BUG=http://code.google.com/p/chromium/issues/detail?id=10611
    
    
    Review URL: http://codereview.chromium.org/92075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14330 0039d316-1c4b-4281-b951-d872f2087c98

--- base/stats_table_unittest.cc
+++ base/stats_table_unittest.cc
@@ -108,9 +108,10 @@ void StatsTableThread::Run() {
 }
 
 // Create a few threads and have them poke on their counters.
+// Currently disabled. See bug report below:
 // TODO(maruel): http://crbug.com/10611
 TEST_F(StatsTableTest, MultipleThreads) {
-#if !defined(OS_MAC)
+#if 0
   // Create a stats table.
   const std::string kTableName = "MultipleThreadStatTable";
   const int kMaxThreads = 20;
@@ -163,8 +164,6 @@ TEST_F(StatsTableTest, MultipleThreads) {
   EXPECT_EQ(0, table.CountThreadsRegistered());
 
   DeleteShmem(kTableName);
-#else  // !defined(OS_MAC)
-  NOT_IMPLEMENTED();
 #endif
 }
 
