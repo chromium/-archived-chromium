34e9ea4d62d7142c4817cb77f0785ed85645f373
jcampan@chromium.org <jcampan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-24 22:11:12 +0000

    This CL makes sure we unregister our Windows window classes when shut-down. It also balances-out an OLE initialization performed by the CRichEditCTRL.
    This is necessary for allowing to reload chrome.dll in a process, which is what the browser tests will do.
    
    BUG=None
    TEST=None
    Review URL: http://codereview.chromium.org/93026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14489 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_pump_win.cc
+++ base/message_pump_win.cc
@@ -88,6 +88,7 @@ MessagePumpForUI::MessagePumpForUI() {
 
 MessagePumpForUI::~MessagePumpForUI() {
   DestroyWindow(message_hwnd_);
+  UnregisterClass(kWndClass, GetModuleHandle(NULL));
 }
 
 void MessagePumpForUI::ScheduleWork() {
