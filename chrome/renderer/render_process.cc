04019e2194e872e61d1c1fa2f6065bc5bbe85c55
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-27 20:34:31 +0000

    linux: make --single-process mean in-process renderer.
    Since we can't have in-process plugins on Linux, let's make
    single-process do as much of a single process as possible.
    
    The alternative is detecting and erroring out on --single-process, then
    adding a new flag and changing all of the other code that currently
    examines --single-process to examine both the new flag and
    --single-process.  That seems like a lot of extra code and source of
    potential error.
    
    Note that we print out a warning indicating this on --single-process
    in the LINUX case.
    
    Review URL: http://codereview.chromium.org/100053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14645 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_process.cc
+++ chrome/renderer/render_process.cc
@@ -116,8 +116,16 @@ void RenderProcess::Init() {
 
 bool RenderProcess::InProcessPlugins() {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+#if defined(OS_LINUX)
+  // Plugin processes require a UI message loop, and the Linux message loop
+  // implementation only allows one UI loop per process.
+  if (command_line.HasSwitch(switches::kInProcessPlugins))
+    NOTIMPLEMENTED() << ": in process plugins not supported on Linux";
+  return command_line.HasSwitch(switches::kInProcessPlugins);
+#else
   return command_line.HasSwitch(switches::kInProcessPlugins) ||
          command_line.HasSwitch(switches::kSingleProcess);
+#endif
 }
 
 // -----------------------------------------------------------------------------
