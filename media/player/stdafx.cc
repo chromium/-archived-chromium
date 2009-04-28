725cfd61d2e1db9e58b2ea2350524c274e5fa218
fbarchard@chromium.org <fbarchard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-28 00:42:15 +0000

    stdafx for precompiled stdafx generation in media player.
    Review URL: http://codereview.chromium.org/100074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14694 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ media/player/stdafx.cc
@@ -0,0 +1,13 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.  Use of this
+// source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+
+// stdafx.cpp : Source file that includes just the standard includes.
+// This file is in Microsoft coding style.
+
+#include "media/player/stdafx.h"
+
+#if (_ATL_VER < 0x0700)
+#include <atlimpl.cpp>
+#endif
+
