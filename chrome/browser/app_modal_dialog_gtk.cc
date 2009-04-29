29270ff8b304b53f794f975244bdfb86cdcdcb6c
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-29 18:16:43 +0000

    Free gtk list.  Should fix valgrind warning
    24 (12 direct, 12 indirect) bytes in 1 blocks are definitely lost in loss record 54 of 258 
       at malloc (vg_replace_malloc.c:178)
       ...
       by gtk_container_foreach (in /usr/lib/libgtk-x11-2.0.so.0.1200.9)
       by gtk_container_get_children (in /usr/lib/libgtk-x11-2.0.so.0.1200.9)
       by (anonymous namespace)::OnDialogResponse(_GtkDialog*, int, AppModalDialog*) (app_modal_dialog_gtk.cc:24)
    
    Review URL: http://codereview.chromium.org/102007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14855 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/app_modal_dialog_gtk.cc
+++ chrome/browser/app_modal_dialog_gtk.cc
@@ -19,15 +19,17 @@ namespace {
 // If there's a text entry in the dialog, get the text from the first one and
 // return it.
 std::wstring GetPromptText(GtkDialog* dialog) {
+  std::wstring text;
   // TODO(tc): Replace with gtk_dialog_get_content_area() when using GTK 2.14+
   GtkWidget* contents_vbox = dialog->vbox;
   GList* first_child = gtk_container_get_children(GTK_CONTAINER(contents_vbox));
   for (GList* item = first_child; item; item = g_list_next(item)) {
     if (GTK_IS_ENTRY(item->data)) {
-      return UTF8ToWide(gtk_entry_get_text(GTK_ENTRY(item->data)));
+      text = UTF8ToWide(gtk_entry_get_text(GTK_ENTRY(item->data)));
     }
   }
-  return std::wstring();
+  g_list_free(first_child);
+  return text;
 }
 
 void OnDialogResponse(GtkDialog* dialog, gint response_id,
