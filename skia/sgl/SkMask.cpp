53d85a84f13333af50823c5a5bfb6b0eeaab6c46
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-30 12:50:58 +0000

    Integrate change of Skia upstream. This is a cherry-pick of the following change:
    http://code.google.com/p/skia/source/detail?r=159
    
    We introduce this change for the 'lighter' composite operation. We need kAdd_Mode, which is introduced in this change for the operation.
    
    Patch by Shinichiro Hamaji.
    
    BUG=1619
    
    Review URL: http://codereview.chromium.org/93093
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@14938 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/sgl/SkMask.cpp
+++ skia/sgl/SkMask.cpp
@@ -15,41 +15,29 @@
 ** limitations under the License.
 */
 
-#include <limits>
-
+#include "Sk64.h"
 #include "SkMask.h"
 
-size_t SkMask::computeImageSize() const
-{
-    // Prevent too large a number. There is a better fix for this in Skia
-    // trunk where it returns failure.
-    long long size = (long long)fBounds.height() * (long long)fRowBytes;
-    if (size >= std::numeric_limits<size_t>::max() / 2) {
-#ifdef WIN32
-        __debugbreak();
-#else
-        abort();
-#endif
+/** returns the product if it is positive and fits in 31 bits. Otherwise this
+    returns 0.
+ */
+static int32_t safeMul32(int32_t a, int32_t b) {
+    Sk64 size;
+    size.setMul(a, b);
+    if (size.is32() && size.isPos()) {
+        return size.get32();
     }
-
-    return size;
+    return 0;
 }
 
-size_t SkMask::computeTotalImageSize() const
-{
+size_t SkMask::computeImageSize() const {
+    return safeMul32(fBounds.height(), fRowBytes);
+}
+
+size_t SkMask::computeTotalImageSize() const {
     size_t size = this->computeImageSize();
-
     if (fFormat == SkMask::k3D_Format) {
-        // See computeImageSize for why we want to stop here.
-        if (size > std::numeric_limits<size_t>::max() / 3) {
-#ifdef WIN32
-            __debugbreak();
-#else
-        abort();
-#endif
-        }
-
-        size *= 3;
+        size = safeMul32(size, 3);
     }
     return size;
 }
