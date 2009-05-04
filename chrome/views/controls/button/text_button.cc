5673d72c31bff9037b0e5621ca56fdb5ad74de6f
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-04 02:52:01 +0000

    Reverting 15183. Review URL: http://codereview.chromium.org/99336
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15185 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/controls/button/text_button.cc
+++ chrome/views/controls/button/text_button.cc
@@ -10,10 +10,10 @@
 #include "chrome/common/throb_animation.h"
 #include "chrome/views/controls/button/button.h"
 #include "chrome/views/event.h"
+#include "grit/views_resources.h"
 
 #if defined(OS_WIN)
 #include "chrome/common/win_util.h"
-#include "grit/views_resources.h"
 #endif
 
 namespace views {
