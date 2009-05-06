5005128a5366d4fae6a6831d75cd4dedfacc0e7d
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-06 00:45:41 +0000

    Fix build bustage by correcting include line - forgot to grab this from my linux box.
    TBR=brettw
    Review URL: http://codereview.chromium.org/113013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15362 0039d316-1c4b-4281-b951-d872f2087c98

--- app/gfx/chrome_canvas_linux.cc
+++ app/gfx/chrome_canvas_linux.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/common/gfx/chrome_canvas.h"
+#include "app/gfx/chrome_canvas.h"
 
 #include <pango/pango.h>
 
