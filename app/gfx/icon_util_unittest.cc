5708743676ab8deae5f64289d23f8a540f948ec6
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-07 20:32:53 +0000

    Remove unneeded ATL header includes.
    TEST=none
    Review URL: http://codereview.chromium.org/113108
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@15576 0039d316-1c4b-4281-b951-d872f2087c98

--- app/gfx/icon_util_unittest.cc
+++ app/gfx/icon_util_unittest.cc
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <atlbase.h>
-#include <atlapp.h>
-#include <atlmisc.h>
-
 #include "app/gfx/icon_util.h"
 #include "base/gfx/size.h"
 #include "base/scoped_ptr.h"
