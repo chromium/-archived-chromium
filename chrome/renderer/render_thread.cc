30754f877c485d1c193465d7db61075ac2c8e10a
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-21 00:44:37 +0000

    Send the V8 histograms to UMA
    Review URL: http://codereview.chromium.org/115183
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16558 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/render_thread.cc
+++ chrome/renderer/render_thread.cc
@@ -298,7 +298,11 @@ void RenderThread::InformHostOfCacheStatsLater() {
 
 static void* CreateHistogram(
     const char *name, int min, int max, size_t buckets) {
-  return new Histogram(name, min, max, buckets);
+  Histogram* histogram = new Histogram(name, min, max, buckets);
+  if (histogram) {
+    histogram->SetFlags(kUmaTargetedHistogramFlag);
+  }
+  return histogram;
 }
 
 static void AddHistogramSample(void* hist, int sample) {
