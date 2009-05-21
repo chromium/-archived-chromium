f9dc1ffab25f50993024937c02bcc0bfd3592e42
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-21 21:06:39 +0000

    Fix Linux build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@16654 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/debugger_node.cc
+++ chrome/browser/debugger/debugger_node.cc
@@ -122,8 +122,8 @@ v8::Handle<v8::Value> ChromeNode::PropGetter(v8::Handle<v8::String> prop,
     return (new FunctionNode<DebuggerShell>(DebuggerShell::SetDebuggerBreak,
                                             debugger_))->NewInstance();
   }
-  return (prop->Equals(v8::String::New("foo"))) ?
-      static_cast<v8::Handle<v8::Value> >(v8::Undefined()) : prop;
+  return static_cast<v8::Handle<v8::Value> >(
+      prop->Equals(v8::String::New("foo")) ? v8::Undefined() : prop);
 }
 
 /////////////////////////////////////////////
