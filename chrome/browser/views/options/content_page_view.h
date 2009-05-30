923bded91d4c9aa3076551788b4068f0664e3729
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-30 15:21:42 +0000

    Create a "User data" tab and remove the "minor tweaks" tab. The mockup is included in the issue.
    Add new option fields to the "User Data" tab and reorganize the "Under the Hood" to fit a better User Experience.
    
    BUG=11435 (http://crbug.com/11435)
    TEST=Opened the option dialog and checked if functional.
    
    Review URL: http://codereview.chromium.org/113896
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17288 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/options/content_page_view.h
+++ chrome/browser/views/options/content_page_view.h
@@ -1,12 +1,11 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_VIEWS_OPTIONS_CONTENT_PAGE_VIEW_H__
-#define CHROME_BROWSER_VIEWS_OPTIONS_CONTENT_PAGE_VIEW_H__
+#ifndef CHROME_BROWSER_VIEWS_OPTIONS_CONTENT_PAGE_VIEW_H_
+#define CHROME_BROWSER_VIEWS_OPTIONS_CONTENT_PAGE_VIEW_H_
 
 #include "chrome/browser/views/options/options_page_view.h"
-#include "chrome/browser/shell_dialogs.h"
 #include "chrome/common/pref_member.h"
 #include "views/controls/button/button.h"
 #include "views/view.h"
@@ -24,20 +23,13 @@ class PrefService;
 // ContentPageView
 
 class ContentPageView : public OptionsPageView,
-                        public views::ButtonListener,
-                        public SelectFileDialog::Listener {
+                        public views::ButtonListener {
  public:
   explicit ContentPageView(Profile* profile);
   virtual ~ContentPageView();
 
   // views::ButtonListener implementation:
   virtual void ButtonPressed(views::Button* sender);
-
-  // SelectFileDialog::Listener implementation:
-  virtual void FileSelected(const FilePath& path, int index, void* params);
-
-  // OptionsPageView implementation:
-  virtual bool CanClose() const;
 
  protected:
   // OptionsPageView implementation:
@@ -49,33 +41,17 @@ class ContentPageView : public OptionsPageView,
 
  private:
   // Init all the dialog controls.
-  void InitDownloadLocation();
   void InitPasswordSavingGroup();
-  void InitFontsLangGroup();
   void InitFormAutofillGroup();
+  void InitImportGroup();
+  void InitClearDataGroup();
   void InitThemesGroup();
-
-  // Updates the directory displayed in the default download location view with
-  // the current value of the pref.
-  void UpdateDownloadDirectoryDisplay();
-
-  // Controls for the Download Location group.
-  OptionsGroupView* download_location_group_;
-  FileDisplayArea* download_default_download_location_display_;
-  views::NativeButton* download_browse_button_;
-  views::Checkbox* download_ask_for_save_location_checkbox_;
-  scoped_refptr<SelectFileDialog> select_file_dialog_;
 
   // Controls for the Password Saving group
   views::NativeButton* passwords_exceptions_button_;
   OptionsGroupView* passwords_group_;
   views::RadioButton* passwords_asktosave_radio_;
   views::RadioButton* passwords_neversave_radio_;
-
-  // Controls for the Fonts and Languages group.
-  OptionsGroupView* fonts_lang_group_;
-  views::Label* fonts_and_languages_label_;
-  views::NativeButton* change_content_fonts_button_;
 
   // Controls for the Form Autofill group
   OptionsGroupView* form_autofill_group_;
@@ -90,12 +66,20 @@ class ContentPageView : public OptionsPageView,
   views::RadioButton* popups_show_minimized_radio_;
   views::RadioButton* popups_block_all_radio_;
 
-  StringPrefMember default_download_location_;
-  BooleanPrefMember ask_for_save_location_;
+  // Controls for the Import bookmarks and settings group.
+  OptionsGroupView* import_group_;
+  views::Label* import_label_;
+  views::NativeButton* import_button_;
+
+  // Controls for Clear browsing data group.
+  OptionsGroupView* clear_data_group_;
+  views::Label* clear_data_label_;
+  views::NativeButton* clear_data_button_;
+
   BooleanPrefMember ask_to_save_passwords_;
   BooleanPrefMember form_autofill_;
 
-  DISALLOW_EVIL_CONSTRUCTORS(ContentPageView);
+  DISALLOW_COPY_AND_ASSIGN(ContentPageView);
 };
 
-#endif  // #ifndef CHROME_BROWSER_VIEWS_OPTIONS_CONTENT_PAGE_VIEW_H__
+#endif  // CHROME_BROWSER_VIEWS_OPTIONS_CONTENT_PAGE_VIEW_H_
