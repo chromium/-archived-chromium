77019c86522f3b09bfec291b319566a5e70d480c
slightlyoff@chromium.org <slightlyoff@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-05 00:59:05 +0000

    Disabling a windows-only test failure to turn the tree green again and re-open. Evmar, willchan, and I are still investigating.
    TEST=none
    
    BUG=13388
    
    Review URL: http://codereview.chromium.org/119219
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/url_fetcher_unittest.cc
+++ chrome/browser/net/url_fetcher_unittest.cc
@@ -372,7 +372,7 @@ TEST_F(URLFetcherPostTest, Basic) {
   MessageLoop::current()->Run();
 }
 
-TEST_F(URLFetcherHeadersTest, Headers) {
+TEST_F(URLFetcherHeadersTest, DISABLED_Headers) {
   scoped_refptr<HTTPTestServer> server =
       HTTPTestServer::CreateServer(L"net/data/url_request_unittest", NULL);
   ASSERT_TRUE(NULL != server.get());
