1e1c05b77a95a8a06e5c65c0ebe483713e88b22d
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-09 03:53:19 +0000

    Fix toolkit_views bustage by updating callsite to use the new function name.
    BUG=none
    TEST=none
    TBR=jcampan
    Review URL: http://codereview.chromium.org/118419
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@17929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/tab_contents/native_tab_contents_container_gtk.cc
+++ chrome/browser/views/tab_contents/native_tab_contents_container_gtk.cc
@@ -167,10 +167,11 @@ void NativeTabContentsContainerGtk::AboutToRequestFocusFromTabTraversal(
     return;
   // Give an opportunity to the tab to reset its focus.
   if (container_->tab_contents()->interstitial_page()) {
-    container_->tab_contents()->interstitial_page()->SetInitialFocus(reverse);
+    container_->tab_contents()->interstitial_page()->FocusThroughTabTraversal(
+        reverse);
     return;
   }
-  container_->tab_contents()->SetInitialFocus(reverse);
+  container_->tab_contents()->FocusThroughTabTraversal(reverse);
 }
 
 bool NativeTabContentsContainerGtk::GetAccessibleRole(
