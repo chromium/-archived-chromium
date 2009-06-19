fcd4ddc59912c6e84b9e8bdbfe96d288c5663f2b
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-19 22:32:51 +0000

    Reverting 18865. Review URL: http://codereview.chromium.org/140020
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@18869 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/views/autocomplete/autocomplete_popup_contents_view.h
+++ chrome/browser/views/autocomplete/autocomplete_popup_contents_view.h
@@ -9,12 +9,12 @@
 #include "chrome/browser/autocomplete/autocomplete.h"
 #include "chrome/browser/autocomplete/autocomplete_popup_model.h"
 #include "chrome/browser/autocomplete/autocomplete_popup_view.h"
+#include "chrome/browser/views/autocomplete/autocomplete_popup_win.h"
 #include "views/view.h"
 #include "webkit/glue/window_open_disposition.h"
 
 class AutocompleteEditModel;
 class AutocompleteEditViewWin;
-class AutocompletePopupWin;
 class Profile;
 
 // An interface implemented by an object that provides data to populate
@@ -91,10 +91,8 @@ class AutocompletePopupContentsView : public views::View,
   // Makes the contents of the canvas slightly transparent.
   void MakeCanvasTransparent(gfx::Canvas* canvas);
 
-#if defined(OS_WIN)
   // The popup that contains this view.
   scoped_ptr<AutocompletePopupWin> popup_;
-#endif
 
   // The provider of our result set.
   scoped_ptr<AutocompletePopupModel> model_;
