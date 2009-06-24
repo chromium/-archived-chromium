b91dd52c6d15f8a14216286684b8df74cc70032c
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-24 21:01:36 +0000

    Fix test (really, you call this a test?) that I broke in r19167 Review URL: http://codereview.chromium.org/147105
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19169 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/cocoa/tab_controller_unittest.mm
+++ chrome/browser/cocoa/tab_controller_unittest.mm
@@ -126,9 +126,9 @@ TEST_F(TabControllerTest, Loading) {
   scoped_nsobject<TabController> controller([[TabController alloc] init]);
   [[window contentView] addSubview:[controller view]];
 
-  EXPECT_FALSE([controller loading]);
-  [controller setLoading:YES];
-  EXPECT_TRUE([controller loading]);
+  EXPECT_EQ(DONE, [controller loadingState]);
+  [controller setLoadingState:LOADING];
+  EXPECT_EQ(LOADING, [controller loadingState]);
 
   [[controller view] removeFromSuperview];
 }
