b7ce2fe606a93913d15f09bc0727bb0a17943dc2
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 04:36:11 +0000

    Fixes bug in RootViewGtk where we we might not paint correctly if SchedulePaint is invoked consecutively with different regions. Windows has similar code.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/147170
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19338 0039d316-1c4b-4281-b951-d872f2087c98

--- views/widget/root_view_gtk.cc
+++ views/widget/root_view_gtk.cc
@@ -13,8 +13,19 @@
 namespace views {
 
 void RootView::OnPaint(GdkEventExpose* event) {
-  gfx::CanvasPaint canvas(event);
+  gfx::Rect original_dirty_region = GetScheduledPaintRectConstrainedToSize();
+  if (!original_dirty_region.IsEmpty()) {
+    // Between the the time the paint was scheduled and the time we end
+    // up painting more SchedulePaints may have been invoked. Expand the
+    // region Gdk wants us to paint to include the region we want to paint
+    // to make sure everything is painted. Otherwise we may not paint
+    // everything we need to.
+    gfx::Rect complete_area =
+        original_dirty_region.Union(gfx::Rect(event->area));
+    event->area = complete_area.ToGdkRectangle();
+  }
 
+  gfx::CanvasPaint canvas(event);
   if (!canvas.is_empty()) {
     canvas.set_composite_alpha(
         static_cast<WidgetGtk*>(GetWidget())->is_transparent());
