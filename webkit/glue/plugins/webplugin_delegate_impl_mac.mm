08998ed15e5107700c20b18ea4a0eb7b0d7fefce
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-26 00:41:42 +0000

    mac: build fix.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19322 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/webplugin_delegate_impl_mac.mm
+++ webkit/glue/plugins/webplugin_delegate_impl_mac.mm
@@ -58,7 +58,7 @@ WebPluginDelegateImpl* g_current_plugin_instance = NULL;
 WebPluginDelegate* WebPluginDelegate::Create(
     const FilePath& filename,
     const std::string& mime_type,
-    gfx::NativeView containing_view) {
+    gfx::PluginWindowHandle containing_view) {
   scoped_refptr<NPAPI::PluginLib> plugin =
       NPAPI::PluginLib::CreatePluginLib(filename);
   if (plugin.get() == NULL)
@@ -73,12 +73,13 @@ WebPluginDelegate* WebPluginDelegate::Create(
   return new WebPluginDelegateImpl(containing_view, instance.get());
 }
 
-bool WebPluginDelegateImpl::IsPluginDelegateWindow(gfx::NativeWindow window) {
+bool WebPluginDelegateImpl::IsPluginDelegateWindow(
+    gfx::PluginNativeView window) {
   return false;
 }
 
 bool WebPluginDelegateImpl::GetPluginNameFromWindow(
-    gfx::NativeWindow window, std::wstring *plugin_name) {
+    gfx::PluginNativeView window, std::wstring *plugin_name) {
   if (NULL == plugin_name) {
     return false;
   }
