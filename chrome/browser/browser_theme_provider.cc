f2afcf52c2a3903e74174e9e54ff667602149abf
cmp@google.com <cmp@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-02 02:12:14 +0000

    Prefer arraysize() over sizeof().
    Fix a call to sizeof() on a static array to use
    arraysize() instead.  Maybe fixes a coverity issue,
    too.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/150202
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@19800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_theme_provider.cc
+++ chrome/browser/browser_theme_provider.cc
@@ -125,7 +125,7 @@ BrowserThemeProvider::BrowserThemeProvider()
     : rb_(ResourceBundle::GetSharedInstance()) {
   static bool initialized = false;
   if (!initialized) {
-    for (size_t i = 0; i < sizeof(kToolbarButtonIDs); ++i) {
+    for (size_t i = 0; i < arraysize(kToolbarButtonIDs); ++i) {
       button_images_[kToolbarButtonIDs[i]] = true;
     }
     frame_tints_[IDR_THEME_FRAME] = TINT_FRAME;
