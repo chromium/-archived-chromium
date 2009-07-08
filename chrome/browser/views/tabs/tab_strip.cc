chromium/-archived-chromium

commit 8018886ffd8aea38b774a767e2ceab412f85f715

Theme window control buttons.
BUG= http://crbug.com/14579
TEST= Install a theme with themed window control buttons.  Observe that WCB's are themed.


Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=19758
Review URL: http://codereview.chromium.org/150041

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20028 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/views/tabs/tab_strip.cc
@@ -16,6 +16,7 @@
 #endif
 #include "base/gfx/size.h"
 #include "base/stl_util-inl.h"
+#include "chrome/browser/browser_theme_provider.h"
 #include "chrome/browser/metrics/user_metrics.h"
 #include "chrome/browser/profile.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
@@ -29,6 +30,7 @@
 #include "grit/theme_resources.h"
 #include "views/controls/image_view.h"
 #include "views/painter.h"
+#include "views/widget/default_theme_provider.h"
 #include "views/window/non_client_view.h"
 #include "views/window/window.h"
 
@@ -595,6 +597,13 @@ void TabStrip::SetBackgroundOffset(gfx::Point offset) {
     GetTabAt(i)->SetBackgroundOffset(offset);
 }
 
+void TabStrip::InitTabStripButtons() {
+  newtab_button_ = new NewTabButton(this);
+  LoadNewTabButtonImage();
+  newtab_button_->SetAccessibleName(l10n_util::GetString(IDS_ACCNAME_NEWTAB));
+  AddChildView(newtab_button_);
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // TabStrip, views::View overrides:
 
@@ -766,6 +775,10 @@ views::View* TabStrip::GetViewForPoint(const gfx::Point& point) {
 
   // No need to do any floating view stuff, we don't use them in the TabStrip.
   return this;
+}
+
+void TabStrip::ThemeChanged() {
+  LoadNewTabButtonImage();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -1075,29 +1088,35 @@ void TabStrip::DidProcessEvent(GdkEvent* event) {
 
 void TabStrip::Init() {
   model_->AddObserver(this);
-  newtab_button_ = new NewTabButton(this);
-  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
-  SkBitmap* bitmap;
-
-  bitmap = rb.GetBitmapNamed(IDR_NEWTAB_BUTTON);
-  newtab_button_->SetImage(views::CustomButton::BS_NORMAL, bitmap);
-  newtab_button_->SetImage(views::CustomButton::BS_PUSHED,
-                           rb.GetBitmapNamed(IDR_NEWTAB_BUTTON_P));
-  newtab_button_->SetImage(views::CustomButton::BS_HOT,
-                           rb.GetBitmapNamed(IDR_NEWTAB_BUTTON_H));
-
-  newtab_button_size_.SetSize(bitmap->width(), bitmap->height());
-  actual_newtab_button_size_ = newtab_button_size_;
-
-  newtab_button_->SetAccessibleName(l10n_util::GetString(IDS_ACCNAME_NEWTAB));
-  AddChildView(newtab_button_);
-
+  newtab_button_size_.SetSize(kNewTabButtonWidth, kNewTabButtonHeight);
   if (drop_indicator_width == 0) {
     // Direction doesn't matter, both images are the same size.
     SkBitmap* drop_image = GetDropArrowImage(true);
     drop_indicator_width = drop_image->width();
     drop_indicator_height = drop_image->height();
   }
+}
+
+void TabStrip::LoadNewTabButtonImage() {
+  ThemeProvider* tp = GetThemeProvider();
+
+  // If we don't have a theme provider yet, it means we do not have a
+  // root view, and are therefore in a test.
+  if (tp == NULL)
+    tp = new views::DefaultThemeProvider();
+
+  SkBitmap* bitmap = tp->GetBitmapNamed(IDR_NEWTAB_BUTTON);
+  SkColor color = tp->GetColor(BrowserThemeProvider::COLOR_BUTTON_BACKGROUND);
+  SkBitmap* background = tp->GetBitmapNamed(
+      IDR_THEME_WINDOW_CONTROL_BACKGROUND);
+
+  newtab_button_->SetImage(views::CustomButton::BS_NORMAL, bitmap);
+  newtab_button_->SetImage(views::CustomButton::BS_PUSHED,
+                           tp->GetBitmapNamed(IDR_NEWTAB_BUTTON_P));
+  newtab_button_->SetImage(views::CustomButton::BS_HOT,
+                           tp->GetBitmapNamed(IDR_NEWTAB_BUTTON_H));
+  newtab_button_->SetBackground(color, background,
+                                tp->GetBitmapNamed(IDR_NEWTAB_BUTTON_MASK));
 }
 
 Tab* TabStrip::GetTabAt(int index) const {

chrome/app/theme/theme_resources.grd
@@ -284,13 +284,20 @@
       <include name="IDR_THEME_FRAME_OVERLAY" file="notused.png" type="BINDATA" />
       <include name="IDR_THEME_BUTTON_BACKGROUND" file="notused.png" type="BINDATA" />
       <include name="IDR_THEME_NTP_ATTRIBUTION" file="notused.png" type="BINDATA" />
+      <include name="IDR_THEME_WINDOW_CONTROL_BACKGROUND" file="notused.png" type="BINDATA" />
 
       <include name="IDR_FORWARD_MASK" file="forward_mask.png" type="BINDATA" />
       <include name="IDR_BACK_MASK" file="back_mask.png" type="BINDATA" />
       <include name="IDR_BUTTON_MASK" file="button_mask.png" type="BINDATA" />
       <include name="IDR_STAR_MASK" file="star_mask.png" type="BINDATA" />
       <include name="IDR_GO_MASK" file="go_mask.png" type="BINDATA" />
-      
+
+      <include name="IDR_NEWTAB_BUTTON_MASK" file="newtab_button_mask.png" type="BINDATA" />
+      <include name="IDR_CLOSE_BUTTON_MASK" file="close_button_mask.png" type="BINDATA" />
+      <include name="IDR_RESTORE_BUTTON_MASK" file="restore_button_mask.png" type="BINDATA" />
+      <include name="IDR_MINIMIZE_BUTTON_MASK" file="minimize_button_mask.png" type="BINDATA" />
+      <include name="IDR_MAXIMIZE_BUTTON_MASK" file="maximize_button_mask.png" type="BINDATA" />
+
       <if expr="pp_ifdef('_google_chrome')">
         <include name="IDR_ABOUT_BACKGROUND" file="google_chrome/about_background.png" type="BINDATA" />
         <include name="IDR_ABOUT_BACKGROUND_RTL" file="google_chrome/about_background_rtl.png" type="BINDATA" />

chrome/browser/views/frame/browser_view.cc
@@ -1420,6 +1420,7 @@ void BrowserView::Init() {
   tabstrip_ = new TabStrip(browser_->tabstrip_model());
   tabstrip_->SetAccessibleName(l10n_util::GetString(IDS_ACCNAME_TABSTRIP));
   AddChildView(tabstrip_);
+  tabstrip_->InitTabStripButtons();
   frame_->TabStripCreated(tabstrip_);
 
   toolbar_ = new ToolbarView(browser_.get());

commit 0ec8a8e3bab7f75b0f2b2e791e0a8da7d5526bf1

Fix memory leaks introduced and exposed by r20028.
BUG= none
TEST= none


Review URL: http://codereview.chromium.org/155195

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20129 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/views/tabs/tab_strip.cc
@@ -1102,8 +1102,11 @@ void TabStrip::LoadNewTabButtonImage() {
 
   // If we don't have a theme provider yet, it means we do not have a
   // root view, and are therefore in a test.
-  if (tp == NULL)
+  bool in_test = false;
+  if (tp == NULL) {
     tp = new views::DefaultThemeProvider();
+    in_test = true;
+  }
 
   SkBitmap* bitmap = tp->GetBitmapNamed(IDR_NEWTAB_BUTTON);
   SkColor color = tp->GetColor(BrowserThemeProvider::COLOR_BUTTON_BACKGROUND);
@@ -1117,6 +1120,8 @@ void TabStrip::LoadNewTabButtonImage() {
                            tp->GetBitmapNamed(IDR_NEWTAB_BUTTON_H));
   newtab_button_->SetBackground(color, background,
                                 tp->GetBitmapNamed(IDR_NEWTAB_BUTTON_MASK));
+  if (in_test)
+    delete tp;
 }
 
 Tab* TabStrip::GetTabAt(int index) const {