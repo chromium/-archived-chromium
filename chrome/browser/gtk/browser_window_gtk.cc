6bb2bd9f809eb161d03e56512bd87145c9e78eec
tc@google.com <tc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-08 23:01:03 +0000

    XFCE disables the system decorations if there's an xshape set.  So only use the KWin hack when the custom frame is enabled.
    This is still a bit buggy in KWin (toggling from custom frame -> system
    decorations doesn't always re-attach system decorations), but it seems
    like the best I can do.
    
    Review URL: http://codereview.chromium.org/155248
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -1118,12 +1118,16 @@ void BrowserWindowGtk::UpdateWindowShape(int width, int height) {
     gtk_alignment_set_padding(GTK_ALIGNMENT(window_container_), 1,
         kFrameBorderThickness, kFrameBorderThickness, kFrameBorderThickness);
   } else {
-    // Disable rounded corners.  Simply passing in a NULL region doesn't
-    // seem to work on KWin, so manually set the shape to the whole window.
-    GdkRectangle rect = { 0, 0, width, height };
-    GdkRegion* mask = gdk_region_rectangle(&rect);
-    gdk_window_shape_combine_region(GTK_WIDGET(window_)->window, mask, 0, 0);
-    gdk_region_destroy(mask);
+    if (use_custom_frame_.GetValue()) {
+      // Disable rounded corners.  Simply passing in a NULL region doesn't
+      // seem to work on KWin, so manually set the shape to the whole window.
+      GdkRectangle rect = { 0, 0, width, height };
+      GdkRegion* mask = gdk_region_rectangle(&rect);
+      gdk_window_shape_combine_region(GTK_WIDGET(window_)->window, mask, 0, 0);
+      gdk_region_destroy(mask);
+    } else {
+      gdk_window_shape_combine_region(GTK_WIDGET(window_)->window, NULL, 0, 0);
+    }
     gtk_alignment_set_padding(GTK_ALIGNMENT(window_container_), 0, 0, 0, 0);
   }
 }
